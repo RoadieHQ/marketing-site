186f0029d34ce8b3abdd58b2c106d037baa50493
David Tuite <dtuite@gmail.com>
2020-07-10 17:02:49 +0100

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-166771003-3',
       },
     },
     `gatsby-plugin-feed`,
