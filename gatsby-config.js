21ce76bfdbca00e64d24eec686659e039d4e71d1
David Tuite <dtuite@gmail.com>
2020-07-10 16:59:39 +0100

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/roadie-r-764x764.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
