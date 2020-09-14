15425ab725e9f366a38dd86dfd534dd25464baae
David Tuite <david@roadie.io>
2020-09-14 15:15:44 +0100

    Separate form for subscribing to newsletter

--- src/contactFormConstants.js
+++ src/contactFormConstants.js
@@ -1,4 +1,7 @@
 // This is loaded in a NodeJS process.
 // DO NOT put this file in the src/pages directory. Gatsby will complain because it doesn't export
 // a React component.
-exports.FORM_NAME = 'landing-page/notify-me';
+exports.FORM_NAMES = {
+  notifyMe: 'landing-page/notify-me',
+  subscribeToNewsletter: 'subscribe-to-newsletter',
+};
