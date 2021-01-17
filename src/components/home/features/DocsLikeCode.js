f862322a41aa0a236b195f8bc29994215054706c
David Tuite <david@roadie.io>
2021-01-17 20:50:48 +0000

    Display feature block images on mobile

--- src/components/home/features/DocsLikeCode.js
+++ src/components/home/features/DocsLikeCode.js
@@ -1,54 +1,51 @@
 import React from 'react';
-import { TwoColumnLayout } from 'components';
 import { createUseStyles } from 'react-jss';
 
 import BackgroundImage from '../BackgroundImage';
+import FeatureBlock from '../FeatureBlock';
 
 const useStyles = createUseStyles(() => ({
   p: {
     fontSize: '2rem',
   },
+
+  backgroundImage: {
+    height: 300,
+  },
 }));
 
-const Text = () => {
-  const classes = useStyles();
-
-  return (
-    <div>
-      <h2>Docs that get read</h2>
-      <p className={classes.p}>
-        <span role="img" aria-label="one">
-          1️⃣
-        </span>{' '}
-        Commit markdown docs and API specs alongside your code.
-      </p>
-      <p className={classes.p}>
-        <span role="img" aria-label="two">
-          2️⃣
-        </span>{' '}
-        Beautiful technical documentation appears with the services in your catalog.
-      </p>
-    </div>
-  );
-};
+const Text = ({ classes }) => (
+  <div>
+    <h2>Docs that get read</h2>
+    <p className={classes.p}>
+      <span role="img" aria-label="one">
+        1️⃣
+      </span>
+      {'  '}
+      Commit markdown docs and API specs alongside your code.
+    </p>
+    <p className={classes.p}>
+      <span role="img" aria-label="two">
+        2️⃣
+      </span>
+      {'  '}
+      Beautiful technical documentation appears with the services in your catalog.
+    </p>
+  </div>
+);
 
 const FeatureDocsLikeCode = ({ imageSide = 'right' }) => {
-  const image = <BackgroundImage backgroundImage="url(undraw/undraw_education_f8ru.svg)" />;
-  const text = <Text />;
+  const classes = useStyles();
 
-  if (imageSide === 'right') {
-    return (
-      <div style={{ paddingLeft: 100 }}>
-        <TwoColumnLayout leftContent={text} rightContent={image} />
-      </div>
-    );
-  }
-
-  return (
-    <div style={{ paddingRight: 100 }}>
-      <TwoColumnLayout leftContent={image} rightContent={text} />
-    </div>
+  const image = (
+    <BackgroundImage
+      backgroundImage="url(undraw/undraw_education_f8ru.svg)"
+      className={classes.backgroundImage}
+    />
   );
+  const text = <Text classes={classes} />;
+
+  return <FeatureBlock text={text} image={image} imageSide={imageSide} />;
 };
 
 export default FeatureDocsLikeCode;
