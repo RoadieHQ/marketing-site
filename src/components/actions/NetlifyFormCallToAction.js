f355bb3d27021b660af39b948e3fcf7e9ebe61e0
David Tuite <david@roadie.io>
2021-03-24 15:35:05 +0000

    Extract EmailCaptureForm

--- src/components/actions/NetlifyFormCallToAction.js
+++ src/components/actions/NetlifyFormCallToAction.js
@@ -1,73 +1,8 @@
 import React, { useState } from 'react';
-import { createUseStyles } from 'react-jss';
-import classnames from 'classnames';
-import { FaPaperPlane } from 'react-icons/fa';
 import { trackCustomEvent } from 'gatsby-plugin-google-analytics';
 
-import Button from '../home/Button';
+import EmailCaptureForm from './EmailCaptureForm';
 import { FORM_NAMES } from '../../contactFormConstants';
-
-export const styles = (theme) => ({
-  inputWrapper: {
-    width: '100%',
-    display: 'flex',
-    marginBottom: 8,
-  },
-
-  input: {
-    flex: 1,
-
-    border: 'none',
-    borderLeft: `2px solid ${theme.palette.primary.main}`,
-    borderRadius: 0,
-
-    backgroundColor: theme.palette.grey[100],
-    color: theme.palette.secondary.dark,
-
-    lineHeight: 3,
-    padding: '0.1rem 0.5rem',
-
-    '&:focus': {
-      borderRadius: 0,
-      // Just change the color of the border so the cursor in the input doesn't move.
-      borderLeftColor: 'transparent',
-      outlineWidth: 2,
-      outlineStyle: 'solid',
-      outlineColor: theme.palette.primary.main,
-      // Fixes issue in Firefox where outline is outside the input vs Chrome where it is inside.
-      outlineOffset: -2,
-    },
-
-    '&::placeholder': {
-      color: theme.palette.secondary.light,
-      // Override Firefox's unusual default opacity; see https://github.com/twbs/bootstrap/pull/11526.
-      opacity: 0.5,
-    },
-  },
-
-  subForm: {
-    fontSize: '1.2rem',
-    color: theme.palette.grey[600],
-    minHeight: 16,
-  },
-
-  subFormerror: {
-    color: theme.palette.deepOrange[700],
-  },
-
-  label: {
-    display: 'none',
-  },
-
-  [`@media (min-width: ${theme.breakpoints.values.md}px)`]: {
-    input: {
-      fontSize: '2rem',
-      padding: '0.5rem 0.5rem',
-    },
-  },
-});
-
-const useStyles = createUseStyles(styles);
 
 export const encode = (data) => {
   const formData = new FormData();
@@ -87,7 +22,6 @@ const NetlifyFormCallToAction = ({
   email,
   setEmail,
 }) => {
-  const classes = useStyles();
   const [submitting, setSubmitting] = useState(false);
   const [subForm, setSubForm] = useState({
     message: subFormMessage,
@@ -124,39 +58,20 @@ const NetlifyFormCallToAction = ({
     setSubmitting(false);
   };
 
-  const onInputChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const disabled = submitting || !email || email === '';
-  const subFormStateClass =
-    `subForm${subForm.state}` in classes && classes[`subForm${subForm.state}`];
-
   /* eslint-disable jsx-a11y/no-autofocus */
   return (
-    <form onSubmit={onSubmit}>
-      <div className={classes.inputWrapper}>
-        <input
-          type="email"
-          name="email"
-          id={`${netlifyFormName}-email-input`}
-          aria-label="Work email address"
-          placeholder={placeholderText}
-          className={classes.input}
-          onChange={onInputChange}
-          value={email}
-          autoFocus={autoFocus}
-        />
-
-        <Button
-          text={buttonText}
-          disabled={disabled}
-          icon={<FaPaperPlane />}
-          id={`${netlifyFormName}-email-button`}
-        />
-      </div>
-      <div className={classnames(subFormStateClass, classes.subForm)}>{subForm.message}</div>
-    </form>
+    <EmailCaptureForm
+      onSubmit={onSubmit}
+      submitting={submitting}
+      emailInputId={`${netlifyFormName}-email-input`}
+      buttonId={`${netlifyFormName}-email-button`}
+      subForm={subForm}
+      setEmail={setEmail}
+      placeholderText={placeholderText}
+      email={email}
+      buttonText={buttonText}
+      autoFocus={autoFocus}
+    />
   );
   /* eslint-enable jsx-a11y/no-autofocus */
 };
