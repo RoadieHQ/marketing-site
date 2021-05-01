c06ef380827e945be2e279e133934fb403d91f65
David Tuite <david@roadie.io>
2021-05-01 12:06:44 +0100

    Extract OpenMic from TopBanner

--- src/components/TopBanner/index.js
+++ src/components/TopBanner/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
 import { Link } from 'components';
-import backstageOpenMicLogo from '../../../content/assets/backstage-open-mic-logo.png';
 
 const useStyles = createUseStyles(() => ({
   root: {
@@ -10,12 +9,6 @@ const useStyles = createUseStyles(() => ({
     backgroundColor: '#121212',
     color: 'white',
     padding: '0.75em 16px',
-  },
-
-  image: {
-    height: 16,
-    verticalAlign: 'middle',
-    marginRight: 8,
   },
 
   link: {
@@ -28,17 +21,12 @@ const useStyles = createUseStyles(() => ({
   },
 }));
 
-const TopBanner = () => {
+const TopBanner = ({ to, children }) => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Link to="https://backstage-openmic.com/" className={classes.link}>
-        <img
-          src={backstageOpenMicLogo}
-          alt="The open mic logo. A purple swirl to the right of the capitalized words open mic"
-          className={classes.image}
-        />
-        Join us at the first ever Backstage Open Mic community event on <strong>March 31st</strong>.
+      <Link to={to} className={classes.link}>
+        {children}
       </Link>
     </div>
   );
