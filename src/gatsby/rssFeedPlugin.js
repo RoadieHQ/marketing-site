de61aec219570e389248e73cf3c21c891a7e286a
David Tuite <david@roadie.io>
2021-05-01 21:15:04 +0100

    Add RSS feed for blog

---
+++ src/gatsby/rssFeedPlugin.js
@@ -0,0 +1,58 @@
+module.exports = [{
+  resolve: 'gatsby-plugin-feed',
+  options: {
+    query: `
+      {
+        site {
+          siteMetadata {
+            title
+            description
+            siteUrl
+            site_url: siteUrl
+          }
+        }
+      }
+    `,
+
+    feeds: [{
+      serialize: ({ query: { site, allMarkdownRemark } }) => {
+        return allMarkdownRemark.edges.map((edge) => ({
+          title: edge.node.frontmatter.title,
+          date: edge.node.frontmatter.date,
+          url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+          guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+          custom_elements: [{
+            'content:encoded': edge.node.html,
+          }],
+        }));
+      },
+
+      query: `
+        query {
+          allMarkdownRemark(
+            sort: { fields: [frontmatter___date], order: DESC }
+            filter: { fileAbsolutePath: { regex: "/.+/blog/.+/" } }
+          ) {
+            edges {
+              node {
+                html
+
+                fields {
+                  slug
+                }
+
+                frontmatter {
+                  date
+                  title
+                }
+              }
+            }
+          }
+        }
+      `,
+
+      output: '/blog/rss.xml',
+      title: 'Roadie Blog and Newsletter',
+    }],
+  },
+}];
