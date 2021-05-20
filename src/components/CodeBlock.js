0d202887565d9df8d875ec93050f1ad1fa67e125
David Tuite <david@roadie.io>
2021-05-20 11:14:37 +0100

    Support markdown in CodeBlock intros

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles((theme) => ({
+  intro: theme.preMadeStyles.content,
+}));
+
 
 const CodeBlock = ({ language, code, intro }) => {
+  const classes = useStyles();
+
   return (
     <div>
-      {intro && intro !== '' && <p>{intro.trim()}</p>}
+      {intro && intro !== '' && (
+        <p className={classes.intro} dangerouslySetInnerHTML={{ __html: intro.trim() }} />
+      )}
 
       {code && code !== '' && (
         <div className="gatsby-highlight" data-language={language}>
