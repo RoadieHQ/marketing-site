02470b53e9b22b10d0e6b1c494fd1e8611cba18b
David Tuite <david@roadie.io>
2021-05-23 16:25:26 +0100

    Use dropdown menu in SitewideHeader

--- src/components/SitewideHeader/index.js
+++ src/components/SitewideHeader/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
 import { graphql, useStaticQuery } from 'gatsby';
-import { FaTwitter, FaGithub } from 'react-icons/fa';
-import { TextLink, LayoutControl } from 'components';
+import { Button, LayoutControl } from 'components';
 import { DEFAULT_MAX_WIDTH_BREAKPOINT } from 'components/LayoutControl';
 
-import IconLink from '../IconLink';
 import styles from './styles';
 import NavItemSpacer from './NavItemSpacer';
 import Logo from './Logo';
 import HamburgerMenu from './HamburgerMenu';
-import BackstageLogo from './BackstageLogo';
 import DropdownMenuItem from './DropdownMenuItem';
 
 const useStyles = createUseStyles(styles);
@@ -22,9 +19,6 @@ const SitewideHeader = ({
   const classes = useStyles({ bottomBorder });
   const data = useStaticQuery(query);
 
-  const TWITTER_URL = `https://twitter.com/${data.site.siteMetadata.social.twitter}`;
-  const GITHUB_URL = `https://github.com/${data.site.siteMetadata.social.github}`;
-
   return (
     <div className={classes.root}>
       <LayoutControl maxWidthBreakpoint={maxWidthBreakpoint}>
@@ -34,34 +28,19 @@ const SitewideHeader = ({
           <nav className={classes.fullScreenNav}>
             <span className={classes.textLinkWrapper}>
               <NavItemSpacer>
-                <DropdownMenuItem title="Resources" />
+                <DropdownMenuItem title="Learn" siteMetadata={data.site.siteMetadata} />
               </NavItemSpacer>
 
               <NavItemSpacer>
-                <TextLink
-                  to="/docs/getting-started/getting-started-for-admins/"
-                  text="Documentation"
+                <Button
+                  to="/evaluation-request/"
+                  link={true}
+                  text="Join the waitlist"
+                  id="evaluation-request-link-button"
+                  color="primary"
                 />
               </NavItemSpacer>
             </span>
-
-            <NavItemSpacer>
-              <IconLink to={TWITTER_URL}>
-                <FaTwitter />
-              </IconLink>
-            </NavItemSpacer>
-
-            <NavItemSpacer>
-              <IconLink to={GITHUB_URL}>
-                <FaGithub />
-              </IconLink>
-            </NavItemSpacer>
-
-            <NavItemSpacer>
-              <IconLink to="https://backstage.io">
-                <BackstageLogo />
-              </IconLink>
-            </NavItemSpacer>
           </nav>
 
           <span className={classes.hamburgerMenuWrapper}>
