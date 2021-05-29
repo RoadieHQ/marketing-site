3d3788286e85e14c6e60634871cdfbd98e9bb6cc
David Tuite <david@roadie.io>
2021-05-29 16:50:10 +0100

    Remove cookie consent

--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,6 @@ export { default as SEO } from './seo';
 export { default as Headline } from './Headline';
 export { default as Lead } from './Lead';
 export { default as InterstitialTitle } from './InterstitialTitle';
-export { default as CookieConsent } from './CookieConsent';
 
 export { default as CodeBlock } from './CodeBlock';
 export { default as TwoColumnPoints } from './TwoColumnPoints';
