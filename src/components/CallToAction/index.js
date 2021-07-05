ed49c63b35dfeaeeb15ce0e95ae901f4d2953bd1
David Tuite <david@roadie.io>
2021-07-05 22:12:43 +0100

    Add clicked button text to hero

--- src/components/CallToAction/index.js
+++ src/components/CallToAction/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Button } from 'components';
 
-export const ButtonLinkCallToAction = ({ text = 'Request a  demo', ...props }) => (
-  <Button
-    to="/evaluation-request/"
-    link={true}
-    text={text}
-    id="evaluation-request-link-button"
-    color="primary"
-    {...props}
-  />
-);
+export const ButtonLinkCallToAction = ({ text = 'Request a  demo', ...props }) => {
+  const codedText = encodeURIComponent(text);
+
+  return (
+    <Button
+      to={`/evaluation-request/?clicked_button_label=${codedText}`}
+      link={true}
+      text={text}
+      id="evaluation-request-link-button"
+      color="primary"
+      {...props}
+    />
+  );
+};
