284dbfe66186ec03f9d252e31cae38499eb4f5ce
David Tuite <david@roadie.io>
2021-07-28 12:56:28 +0100

    Fix tests

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -5,6 +5,11 @@ describe('The landing page', () => {
   });
 
   it('has a form which people can use to get an instance', () => {
+    // We have to stub beause netlify forms don't work in this environment.
+    cy.intercept('POST', 'http://localhost:8001', {
+      statusCode: 200,
+    });
+
     cy.visit('');
     cy.get('#get-instance-email-input').type('test@example.com');
     cy.get('#get-instance-email-button').click();
