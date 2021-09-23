8c33d5ece18f47ae55290b8a02c5a27801604336
David Tuite <david@roadie.io>
2021-09-23 15:05:22 +0100

    Fix button width when not fullWidth

--- src/components/forms/Button.js
+++ src/components/forms/Button.js
@@ -10,7 +10,7 @@ const useStyles = createUseStyles((theme) => ({
 
     width: ({ fullWidth }) => {
       if (fullWidth) return '100%';
-      return 'inherit';
+      return 'fit-content';
     },
 
     color: ({ color }) => {
