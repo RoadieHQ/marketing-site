0a6cbfe0db8289b0fe4f7cd087b4b42daa00df0c
David Tuite <david@roadie.io>
2021-09-23 15:52:49 +0100

    Update tests

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -17,9 +17,27 @@ describe('The landing page', () => {
     cy.get('#get-instance-email-input').type('test@example.com');
     // This is a custom CSS checkbox which is technically invisble. Cypress
     // complains so we have to force it.
+    cy.get('#get-instance-sub-to-newsletter-input').uncheck({ force: true });
+    cy.contains('Request a trial').click();
+    cy.contains('Your Backstage experience is on the way');
+  });
+
+  it('has a flow for rejecting users who use unsupported SCMs', () => {
+    // We have to stub beause netlify forms don't work in this environment.
+    cy.intercept('POST', 'http://localhost:8001', {
+      statusCode: 200,
+    });
+
+    cy.visit('');
+    cy.contains('Try it free').click();
+    cy.url().should('contain', '/free-trial/');
+
+    cy.get('#get-instance-email-input').type('test@example.com');
+    // This is a custom CSS checkbox which is technically invisble. Cypress
+    // complains so we have to force it.
     cy.get('#get-instance-scm-bitbucket-cloud-input').check({ force: true });
     cy.get('#get-instance-sub-to-newsletter-input').uncheck({ force: true });
-    cy.contains('Start your trial').click();
-    cy.contains('Our Roadies will get busy');
+    cy.contains('Request a trial').click();
+    cy.contains('Roadie only supports GitHub Cloud for now');
   });
 });
