860aa3b447d92834d38dafece048748ea7d72502
David Tuite <david@roadie.io>
2021-10-07 15:29:34 +0100

    Migrate ContentHeader and Byline

--- src/templates/tailwind/Doc.js
+++ src/templates/tailwind/Doc.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { createUseStyles } from 'react-jss';
-import {
-  SEO,
-  ContentHeader,
-  TextLink as Link,
-} from 'components';
+import { SEO, TextLink as Link } from 'components';
+import ContentHeader from 'components/tailwind/ContentHeader';
 import { TableOfContentsSidebar } from 'components/Sidebar';
 import { Sidebar } from 'components/tailwind/doc';
 import editOnGitHubUrl from '../../editOnGitHubUrl';
@@ -76,19 +73,24 @@ const Doc = ({
       <main className={classes.main}>
         <Sidebar />
 
-        {/*
         <article className={classes.article}>
           <ContentHeader frontmatter={doc.frontmatter} dateKey="lastUpdated" />
 
+        {/*
           <div className={classes.content}>
             <div className={classes.tocWrapper}>
               <h2>Table of Contents</h2>
               <section dangerouslySetInnerHTML={{ __html: doc.tableOfContents }} />
             </div>
           </div>
+        */}
 
-          <section className={classes.content} dangerouslySetInnerHTML={{ __html: doc.html }} />
+          <section
+            className={classes.content}
+            dangerouslySetInnerHTML={{ __html: doc.html }}
+          />
 
+        {/*
           <footer className={classes.articleFooter}>
             <Link
               to={editOnGitHubUrl({ siteMetadata, node: doc, contentSourcePath: '/content/docs' })}
@@ -96,8 +98,10 @@ const Doc = ({
               Edit this page on GitHub
             </Link>
           </footer>
+        */}
         </article>
 
+        {/*
         <TableOfContentsSidebar headings={doc.headings} className={classes.tocSidebar} />
         */}
       </main>
