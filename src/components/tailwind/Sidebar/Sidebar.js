03802544c74bdfd61f73527757517e935c911f1c
David Tuite <david@roadie.io>
2021-10-08 20:00:22 +0100

    Fix sidebar padding

--- src/components/tailwind/Sidebar/Sidebar.js
+++ src/components/tailwind/Sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classnames from 'classnames';
 
 const Sidebar = ({ children, className }) => (
-  <aside className={classnames('md:w-72 border-gray-100 md:border-r-2 md:pt-7 md:pl-3 md:sticky md:top-0 md:h-screen', className)}>
+  <aside className={classnames('md:w-72 border-gray-100 md:border-r-2 md:pt-5 md:pl-3 md:sticky md:top-0 md:h-screen', className)}>
     <div className="md:h-screen">
       {children}
     </div>
