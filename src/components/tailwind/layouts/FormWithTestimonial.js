0cea548e31ec17c908379557271cf82c703fd766
David Tuite <david@roadie.io>
2021-10-25 16:07:48 +0100

    Fix overlaying - svg, text drawer nav

--- src/components/tailwind/layouts/FormWithTestimonial.js
+++ src/components/tailwind/layouts/FormWithTestimonial.js
@@ -16,10 +16,10 @@ const FormWithTestimonial = ({
   <main className="overflow-hidden pb-24 mb-24">
     <div className="bg-warm-gray-50">
       <div className="py-8 lg:py-12">
-        <div className="relative max-w-7xl mx-auto pl-4 pr-8 sm:px-6 lg:px-8">
+        <div className="relative z-10 max-w-7xl mx-auto pl-4 pr-8 sm:px-6 lg:px-8">
           <Headline>{title}</Headline>
 
-          <p className="mt-6 text-xl text-warm-gray-500 max-w-3xl">
+          <p className="mt-6 text-xl text-warm-gray-500 max-w-3xl bg-white">
             {description}
           </p>
         </div>
