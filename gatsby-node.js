c939b4c41cb887042b796a86af7120a4cbf016eb
David Tuite <david@roadie.io>
2021-10-25 18:55:06 +0100

    It lives

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,28 +36,6 @@ exports.createPages = async ({ graphql, actions }) => {
   });
 
   await createPagesFromQuery({
-    templatePath: './src/templates/tailwind/BlogPost.js',
-    query: BLOGS_QUERY,
-    resultName: 'blogs.edges',
-    actions,
-    graphql,
-    processor: ({ node }, component, allEdges, index) => {
-      const previous = index === allEdges.length - 1 ? null : allEdges[index + 1].node;
-      const next = index === 0 ? null : allEdges[index - 1].node;
-
-      return {
-        path: `/tailwind${node.fields.slug}`,
-        component,
-        context: {
-          slug: node.fields.slug,
-          previous,
-          next,
-        },
-      };
-    },
-  });
-
-  await createPagesFromQuery({
     templatePath: './src/templates/Tag.js',
     query: TAGS_QUERY,
     resultName: 'tagsGroup.group',
@@ -65,21 +43,6 @@ exports.createPages = async ({ graphql, actions }) => {
     graphql,
     processor: ({ fieldValue }, component) => ({
       path: `/tags/${kebabCase(fieldValue)}/`,
-      component,
-      context: {
-        tag: fieldValue,
-      },
-    }),
-  });
-
-  await createPagesFromQuery({
-    templatePath: './src/templates/tailwind/Tag.js',
-    query: TAGS_QUERY,
-    resultName: 'tagsGroup.group',
-    actions,
-    graphql,
-    processor: ({ fieldValue }, component) => ({
-      path: `/tailwind/tags/${kebabCase(fieldValue)}/`,
       component,
       context: {
         tag: fieldValue,
@@ -103,21 +66,6 @@ exports.createPages = async ({ graphql, actions }) => {
   });
 
   await createPagesFromQuery({
-    templatePath: './src/templates/tailwind/Plugin.js',
-    query: PLUGINS_QUERY,
-    resultName: 'plugins.edges',
-    actions,
-    graphql,
-    processor: ({ node }, component) => ({
-      path: `/tailwind${node.fields.slug}`,
-      component,
-      context: {
-        slug: node.fields.slug,
-      },
-    }),
-  });
-
-  await createPagesFromQuery({
     templatePath: './src/templates/LegalNotice.js',
     query: LEGAL_NOTICES_QUERY,
     resultName: 'notices.edges',
@@ -125,21 +73,6 @@ exports.createPages = async ({ graphql, actions }) => {
     graphql,
     processor: ({ node }, component) => ({
       path: node.fields.slug,
-      component,
-      context: {
-        slug: node.fields.slug,
-      },
-    }),
-  });
-
-  await createPagesFromQuery({
-    templatePath: './src/templates/tailwind/LegalNotice.js',
-    query: LEGAL_NOTICES_QUERY,
-    resultName: 'notices.edges',
-    actions,
-    graphql,
-    processor: ({ node }, component) => ({
-      path: `/tailwind${node.fields.slug}`,
       component,
       context: {
         slug: node.fields.slug,
@@ -163,21 +96,6 @@ exports.createPages = async ({ graphql, actions }) => {
   });
 
   await createPagesFromQuery({
-    templatePath: './src/templates/tailwind/Doc.js',
-    query: DOCS_QUERY,
-    resultName: 'docs.edges',
-    actions,
-    graphql,
-    processor: ({ node }, component) => ({
-      path: `/tailwind${node.fields.slug}`,
-      component,
-      context: {
-        slug: node.fields.slug,
-      },
-    }),
-  });
-
-  await createPagesFromQuery({
     templatePath: './src/templates/CaseStudy.js',
     query: CASE_STUDIES_QUERY,
     resultName: 'caseStudies.edges',
@@ -189,28 +107,6 @@ exports.createPages = async ({ graphql, actions }) => {
 
       return {
         path: node.fields.slug,
-        component,
-        context: {
-          slug: node.fields.slug,
-          previous,
-          next,
-        },
-      };
-    },
-  });
-
-  await createPagesFromQuery({
-    templatePath: './src/templates/tailwind/CaseStudy.js',
-    query: CASE_STUDIES_QUERY,
-    resultName: 'caseStudies.edges',
-    actions,
-    graphql,
-    processor: ({ node }, component, allEdges, index) => {
-      const previous = index === allEdges.length - 1 ? null : allEdges[index + 1].node;
-      const next = index === 0 ? null : allEdges[index - 1].node;
-
-      return {
-        path: `/tailwind${node.fields.slug}`,
         component,
         context: {
           slug: node.fields.slug,
