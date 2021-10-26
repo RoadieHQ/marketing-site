019e095762b761d80eda172450b9a71e0f91085a
David Tuite <david@roadie.io>
2021-10-26 19:24:59 +0100

    Fix sticky sidebar bug

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -7,11 +7,12 @@ const Sidebar = ({ children, className, side = 'left', sticky = false }) => (
       classnames('md:w-72 border-gray-100', {
         'md:border-r-2': side === 'left',
         'md:border-l-2': side === 'right',
-        'md:sticky md:top-0 md:h-screen': sticky,
       }, className)
     }
   >
-    {children}
+    <div className={classnames({ 'md:sticky md:top-0 md:pt-4': sticky })}>
+      {children}
+    </div>
   </aside>
 );
 
