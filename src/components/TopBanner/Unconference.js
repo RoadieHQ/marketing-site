5b54b0cbdf793219a5272836419e7ea86ed8a9b1
David Tuite <david@roadie.io>
2021-10-26 12:00:38 +0100

    Centralize TopBanner

--- src/components/TopBanner/Unconference.js
+++ src/components/TopBanner/Unconference.js
@@ -3,7 +3,10 @@ import unconferenceLogo from '../../../content/assets/backstage-unconference.png
 import { Link } from 'components';
 
 const Unconference = () => (
-  <Link to="https://hopin.com/events/backstage-users-unconference" className="text-white underline-none hover:underline">
+  <Link
+    to="https://hopin.com/events/backstage-users-unconference"
+    className="text-white underline-none hover:underline"
+  >
     <img
       src={unconferenceLogo}
       alt="The Backstage users unconference logo."
