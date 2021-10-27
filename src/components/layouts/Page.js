3e946f888d8ecf7b9f048e101ae92049e0920afe
David Tuite <david@roadie.io>
2021-10-27 17:19:45 +0100

    Extract <Page /> component

---
+++ src/components/layouts/Page.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { SitewideHeader, SitewideFooter } from 'components';
+import classnames from 'classnames';
+
+const Page = ({ titleDivide = false, children }) => {
+
+  return (
+    <>
+      <SitewideHeader />
+
+      <div className="bg-white pt-16 pb-20 lg:pt-24 lg:pb-28">
+        <div
+          className={classnames('relative max-w-lg mx-auto lg:max-w-7xl px-2 sm:px-6', {
+            'divide-y-2 divide-gray-200': titleDivide,
+          })}
+        >
+          {children}
+        </div>
+      </div>
+
+      <SitewideFooter />
+    </>
+  );
+};
+
+export default Page;
--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -1,1 +1,2 @@
 export { default as FormWithTestimonial } from './FormWithTestimonial';
+export { default as Page } from './Page';
