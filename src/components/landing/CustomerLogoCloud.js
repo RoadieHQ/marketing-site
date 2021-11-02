61fab864dde9ba07ca6781b628d448926d6e0e7a
David Tuite <david@roadie.io>
2021-11-02 09:45:40 +0000

    Add alt text and customer logo heading

--- src/components/landing/CustomerLogoCloud.js
+++ src/components/landing/CustomerLogoCloud.js
@@ -6,7 +6,7 @@ const LogoItem = ({ src, ...rest }) => (
     <picture>
       <source srcSet={src.webp} type="image/webp" />
       <source srcSet={src.png} type="image/png" />
-      <img src={src.png} {...rest} className="h-10" />
+      <img src={src.png} {...rest} className="h-8 sm:h-10" />
     </picture>
   </div>
 );
@@ -15,6 +15,10 @@ const LogoItem = ({ src, ...rest }) => (
 const CustomerLogoCloud = ({ logos }) => (
   <div className="bg-white">
     <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+      <div className="text-center mb-8 sm:mb-12 text-gray-600">
+        <p>Improving engineering effectiveness at top companies</p>
+      </div>
+
       <div className="grid grid-cols-2 gap-8 md:grid-cols-4">
         {logos.map((logo) => (
           <LogoItem {...logo} key={logo.src} />
