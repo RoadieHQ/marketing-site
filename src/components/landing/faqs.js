a9c4e5a04ce7dd112347c2f27f20536904e57843
Nicolas Arnold <nic@roadie.io>
2021-11-22 11:17:19 +0000

    Add FAQ page and add section about status page

--- src/components/landing/faqs.js
+++ src/components/landing/faqs.js
@@ -8,29 +8,35 @@ const FAQ_CONTENT = [{
     "Roadie is SaaS only for the moment. Some Backstage plugins will need a connection back to your infrastructure. We offer a number of secure methods for enabling this.",
   
 }, {
-  id: 1,
+  id: 2,
   question: "Does Roadie support custom plugins?",
   answer:
     "Yes. Roadie provides a private plugin repository which you can publish Backstage plugins too. They can then be manipulated and used just like ordinary open-source Backstage plugins.",
 
 }, {
-  id: 1,
+  id: 3,
   question: "Where is Roadie data stored?",
   answer:
     "All data is stored within the EU and is encrypted at rest using AES-256 encryption algorithm. AWS KMS is used to manage and protect the encryption keys. Data is backed up to AWS S3 at least once per day.",
 
 }, {
-  id: 1,
+  id: 4,
   question: "What level of support does Roadie provide?",
   answer:
     "Every Roadie customer gets a shared Slack or Discord channel. We pride ourselves on our responsiveness. We also deeply value customer feedback and prioritize your requests in our roadmap.",
+},
+{
+  id: 5,
+  question: "Do you have a status page?",
+  answer:
+    "Yes we do! It can be found here: <a href='https://status.roadie.io'>https://status.roadie.io/</a>",
 }];
 
 
 const Item = ({ question, answer }) => (
   <div>
     <dt className="font-semibold text-gray-900">{question}</dt>
-    <dd className="mt-3 text-gray-500">{answer}</dd>
+    <dd className="mt-3 text-gray-500" dangerouslySetInnerHTML={{ __html: answer }}/>
   </div>
 );
 
---
+++ src/pages/faqs.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SEO,
+  SitewideFooter,
+  SitewideHeader,
+} from 'components';
+import {
+  FAQs
+} from 'components/landing';
+
+const Faqs = () => (
+  <>
+    <SEO
+      title={`FAQs | Roadie`}
+      description="Frequently asked questions"
+    />
+    <SitewideHeader />
+    <FAQs/>
+    <SitewideFooter />
+  </>
+);
+
+export default Faqs;
+
