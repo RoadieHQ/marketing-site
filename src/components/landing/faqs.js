a9bed6e2f18d7ff96655bd1a352839dc8bb8c7df
Nicolas Arnold <nic@roadie.io>
2021-11-22 11:35:22 +0000

    Making faqs answers as functions

--- src/components/landing/faqs.js
+++ src/components/landing/faqs.js
@@ -4,39 +4,47 @@ import { Headline } from 'components';
 const FAQ_CONTENT = [{
   id: 1,
   question: "Is Roadie SaaS or on-prem?",
-  answer:
+  answer: () => 
     "Roadie is SaaS only for the moment. Some Backstage plugins will need a connection back to your infrastructure. We offer a number of secure methods for enabling this.",
   
 }, {
   id: 2,
   question: "Does Roadie support custom plugins?",
-  answer:
+  answer: () =>
     "Yes. Roadie provides a private plugin repository which you can publish Backstage plugins too. They can then be manipulated and used just like ordinary open-source Backstage plugins.",
 
 }, {
   id: 3,
   question: "Where is Roadie data stored?",
-  answer:
+  answer: () => 
     "All data is stored within the EU and is encrypted at rest using AES-256 encryption algorithm. AWS KMS is used to manage and protect the encryption keys. Data is backed up to AWS S3 at least once per day.",
 
 }, {
   id: 4,
   question: "What level of support does Roadie provide?",
-  answer:
+  answer: () =>
     "Every Roadie customer gets a shared Slack or Discord channel. We pride ourselves on our responsiveness. We also deeply value customer feedback and prioritize your requests in our roadmap.",
 },
 {
   id: 5,
   question: "Do you have a status page?",
-  answer:
-    "Yes we do! It can be found here: <a href='https://status.roadie.io'>https://status.roadie.io/</a>",
+  answer: () => {
+    return (
+      <p>
+        Yes we do! It can be found here:{' '}
+        <a href='https://status.roadie.io'>
+         https://status.roadie.io/
+        </a>
+      </p>
+    )
+  },
 }];
 
 
 const Item = ({ question, answer }) => (
   <div>
     <dt className="font-semibold text-gray-900">{question}</dt>
-    <dd className="mt-3 text-gray-500" dangerouslySetInnerHTML={{ __html: answer }}/>
+    <dd className="mt-3 text-gray-500">{answer()}</dd>
   </div>
 );
 
