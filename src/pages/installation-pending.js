b7b95aea33e342a880d981f2154e45173697d9ce
sblausten <sam@roadie.io>
2021-12-09 17:14:25 +0100

    Add text about us emailing them

--- src/pages/installation-pending.js
+++ src/pages/installation-pending.js
@@ -2,19 +2,26 @@ import React from 'react';
 import {
   SitewideFooter,
   SitewideHeader,
+  Lead
 } from 'components';
-
 import {
   SimpleCenteredHeading,
 } from 'components/landing';
 
+const lead = "Thank you for installing the Roadie Backstage GitHub app. Your setup is being completed by the Roadie team."
+const message = `We'll email you when it is finished at your github profile's email address. 
+You can also check in on the progress by contacting us at support@roadie.io or via Intercom in the app.`
+
 const GithubAppInstallHoldingPage = () => (
   <>
     <SitewideHeader />
-    <SimpleCenteredHeading
-      headline="App installation pending..."
-      lead="Thank you for installing the Roadie Backstage GitHub app. Your setup is being completed by the Roadie team. You can check in on the progress by contacting us at support@roadie.io"
-    />
+      <SimpleCenteredHeading
+        headline="App installation pending..."
+        lead={lead}
+      />
+    <div className="max-w-4xl mx-auto text-center mb-24 px-8">
+      <Lead>{message}</Lead>
+    </div>
     <SitewideFooter />
   </>
 );
