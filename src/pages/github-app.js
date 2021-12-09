129647210e62d59a50020ae20f508a38c6b63707
sblausten <sam@roadie.io>
2021-12-09 12:18:28 +0100

    Update message so we don't say we'll contact them when its done

--- src/pages/github-app.js
+++ src/pages/github-app.js
@@ -15,7 +15,7 @@ const GithubAppInstallHoldingPage = () => (
 
     <SimpleCenteredHeading
       headline="App Installation Pending..."
-      lead="Thank you for installing the Roadie Backstage GitHub app. Your setup is being completed by the Roadie team. We'll let you know when it is done but you can check in on the progress by contacting us at support@roadie.io"
+      lead="Thank you for installing the Roadie Backstage GitHub app. Your setup is being completed by the Roadie team. You can check in on the progress by contacting us at support@roadie.io"
     />
 
 
