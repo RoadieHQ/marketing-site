eded1e63901e20b18256d9dfa1df892abd73424b
sblausten <sam@roadie.io>
2021-12-09 12:23:58 +0100

    White space

--- src/pages/github-app.js
+++ src/pages/github-app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import {
   SitewideFooter,
   SitewideHeader,
@@ -12,17 +11,12 @@ import {
 const GithubAppInstallHoldingPage = () => (
   <>
     <SitewideHeader />
-
     <SimpleCenteredHeading
       headline="App installation pending..."
       lead="Thank you for installing the Roadie Backstage GitHub app. Your setup is being completed by the Roadie team. You can check in on the progress by contacting us at support@roadie.io"
     />
-
-
-
     <SitewideFooter />
   </>
 );
 
 export default GithubAppInstallHoldingPage;
-
