564cc203ecd85464812fbdacd6a393e9613e0f35
David Tuite <david@roadie.io>
2021-12-27 22:19:37 +0000

    Switch to exo 2 font

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { Link } from 'components';
-import { RoadieHandIcon, RoadieHandAndWordIcon } from 'components/icons';
-
-export const Mark = () => (
-  <div className="flex justify-start lg:w-0 lg:flex-1">
-    <Link to="/">
-      <span className="sr-only">Roadie</span>
-      <RoadieHandIcon />
-    </Link>
-  </div>
-);
+import { RoadieHandAndWordIcon } from 'components/icons';
 
 export const MarkAndWord = () => (
   <div className="flex justify-start lg:w-0 lg:flex-1">
     <Link to="/">
       <span className="sr-only">Roadie</span>
-      <RoadieHandAndWordIcon height={32} width={126} />
+      <RoadieHandAndWordIcon height={32} width={123} />
     </Link>
   </div>
 );
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -2,7 +2,6 @@ export { default as TwitterIcon } from './TwitterIcon';
 export { default as GitHubIcon } from './GitHubIcon';
 export { default as BackstageIcon } from './BackstageIcon';
 export { default as PlayIcon } from './PlayIcon';
-export { default as RoadieHandIcon } from './RoadieHandIcon';
 export { default as RoadieHandAndWordIcon } from './RoadieHandAndWordIcon';
 export { default as RoadieRIcon } from './RoadieRIcon';
 export { default as QuoteIcon } from './QuoteIcon';
