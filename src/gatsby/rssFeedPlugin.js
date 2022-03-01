6fc67e730e74838ffacb0142ab79170ce2eb6f95
David Tuite <david@roadie.io>
2022-03-01 22:19:18 +0000

    Add changelog RSS feed

--- src/gatsby/rssFeedPlugin.js
+++ src/gatsby/rssFeedPlugin.js
@@ -1,5 +1,96 @@
 const get = require('lodash/get');
 
+const blogFeed = {
+  serialize: ({ query: { site, blogs } }) => {
+    return blogs.edges.map(({ node }) => {
+      return {
+        title: node.title,
+        date: node.date,
+        description: get(node, 'description.childMarkdownRemark.rawMarkdownBody'),
+        url: site.siteMetadata.siteUrl + node.slug,
+        guid: site.siteMetadata.siteUrl + node.slug,
+        custom_elements: [{
+          'content:encoded': get(node, 'body.childMarkdownRemark.html'),
+        }],
+      };
+    });
+  },
+
+  query: `
+    query AllBlogPostsForRss {
+      blogs: allContentfulBlogPost(
+        sort: {fields: date, order: DESC}
+        filter: {tags: {ne: "newsletter"}}
+      ) {
+        edges {
+          node {
+            date
+            slug
+            title
+
+            body {
+              childMarkdownRemark {
+                html
+              }
+            }
+
+            description {
+              childMarkdownRemark {
+                rawMarkdownBody
+              }
+            }
+          }
+        }
+      }
+    }
+  `,
+
+  output: '/blog/rss.xml',
+  title: 'Roadie Blog',
+};
+
+const changelogFeed = {
+  serialize: ({ query: { site, changeSets } }) => {
+    return changeSets.edges.map(({ node }) => {
+      return {
+        title: node.title,
+        date: node.releasedAt,
+        description: get(node, 'description.childMarkdownRemark.rawMarkdownBody'),
+        url: site.siteMetadata.siteUrl + `/changelog/`,
+        guid: site.siteMetadata.siteUrl + `/changelog/${node.slug}/`,
+        custom_elements: [{
+          'content:encoded': get(node, 'description.childMarkdownRemark.html'),
+        }],
+      };
+    });
+  },
+
+  query: `
+    query ChangelogForRss {
+      changeSets: allContentfulChangeSet(
+        sort: {fields: releasedAt, order: DESC}
+      ) {
+        edges {
+          node {
+            title
+            slug
+            releasedAt
+            description {
+              childMarkdownRemark {
+                html
+                rawMarkdownBody
+              }
+            }
+          }
+        }
+      }
+    }
+  `,
+
+  output: '/changelog/rss.xml',
+  title: 'Roadie Changelog',
+};
+
 module.exports = [{
   resolve: 'gatsby-plugin-feed',
   options: {
@@ -16,53 +107,6 @@ module.exports = [{
       }
     `,
 
-    feeds: [{
-      serialize: ({ query: { site, blogs } }) => {
-        return blogs.edges.map(({ node }) => {
-          return {
-            title: node.title,
-            date: node.date,
-            description: get(node, 'description.childMarkdownRemark.rawMarkdownBody'),
-            url: site.siteMetadata.siteUrl + node.slug,
-            guid: site.siteMetadata.siteUrl + node.slug,
-            custom_elements: [{
-              'content:encoded': get(node, 'body.childMarkdownRemark.html'),
-            }],
-          };
-        });
-      },
-
-      query: `
-        query AllBlogPostsForRss {
-          blogs: allContentfulBlogPost(
-            sort: {fields: date, order: DESC}
-            filter: {tags: {ne: "newsletter"}}
-          ) {
-            edges {
-              node {
-                date
-                slug
-                title
-
-                body {
-                  childMarkdownRemark {
-                    html
-                  }
-                }
-
-                description {
-                  childMarkdownRemark {
-                    rawMarkdownBody
-                  }
-                }
-              }
-            }
-          }
-        }
-      `,
-
-      output: '/blog/rss.xml',
-      title: 'Roadie Blog and Newsletter',
-    }],
+    feeds: [blogFeed, changelogFeed],
   },
 }];
