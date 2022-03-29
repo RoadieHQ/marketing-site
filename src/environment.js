6b91e606d567c34379822fe119e37126cda0f9ca
David Tuite <david@roadie.io>
2022-03-29 20:30:41 +0100

    Remove unused lodash func

--- src/environment.js
+++ src/environment.js
@@ -1,5 +1,3 @@
-import get from 'lodash/get';
-
 // This env var is set in the gatsby-config.js file.
 export const currentlyExecutingGitBranch = () => {
   return process.env.GATSBY_GIT_BRANCH_NAME || 'NOT_SUPPLIED';
