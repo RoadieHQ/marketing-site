dbcfef9d21a193699be0f01ee87e47f97f6bd25f
David Tuite <david@roadie.io>
2022-03-30 17:25:13 +0100

    Fix orig list item

--- src/components/backstage/plugins/ListItem.js
+++ src/components/backstage/plugins/ListItem.js
@@ -3,11 +3,11 @@ import React from 'react';
 import Attribution from './Attribution';
 import ListItemHeader from './ListItemHeader';
 
-const ListItem = ({ fields: { slug }, frontmatter: { logoImage, humanName, attribution } }) => {
+const ListItem = ({ fields: { slug }, frontmatter: { logoImage, humanName, attribution } }) => (
   <div className="border-2 p-4 text-center" style={{ height: 350 }}>
     <ListItemHeader slug={slug} logoImage={logoImage} humanName={humanName} />
     <Attribution attribution={attribution} />
   </div>
-};
+);
 
 export default ListItem;
