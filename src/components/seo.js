7b58baa002e15f8d54f9d36acd553ca0767c31e9
David Tuite <david@roadie.io>
2022-05-16 11:37:48 +0100

    Rerg meta

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
@@ -35,62 +28,51 @@ const SEO = ({ title, description = '', lang = 'en', meta = [] }) => {
   const ogImageUrl = `${site.siteMetadata.siteUrl}${roadieLogo}`;
   const ogImageAlt = 'The Roadie logo. A cube in isometric projection with 3 fins cut into the right face. The word Roadie is below.';
 
+  const defaultMeta = [{
+    name: `description`,
+    content: metaDescription,
+  }, {
+    property: `og:title`,
+    content: title,
+  }, {
+    property: `og:description`,
+    content: metaDescription,
+  }, {
+    property: `og:type`,
+    content: `website`,
+  }, {
+    property: `og:image`,
+    content: ogImageUrl,
+  }, {
+    name: `twitter:card`,
+    content: `summary`,
+  }, {
+    name: `twitter:site`,
+    content: twitterHandle,
+  }, {
+    name: `twitter:image`,
+    content: ogImageUrl,
+  }, {
+    name: `twitter:image:alt`,
+    content: ogImageAlt,
+  }, {
+    name: `twitter:creator`,
+    content: twitterHandle,
+  }, {
+    name: `twitter:title`,
+    content: title,
+  }, {
+    name: `twitter:description`,
+    content: metaDescription,
+  }];
+
   return (
     <Helmet
       htmlAttributes={{
         lang,
       }}
       title={title}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          property: `og:image`,
-          content: ogImageUrl,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:site`,
-          content: twitterHandle,
-        },
-        {
-          name: `twitter:image`,
-          content: ogImageUrl,
-        },
-        {
-          name: `twitter:image:alt`,
-          content: ogImageAlt,
-        },
-        {
-          name: `twitter:creator`,
-          content: twitterHandle,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
+      meta={defaultMeta.concat(meta)}
     />
   );
 };
