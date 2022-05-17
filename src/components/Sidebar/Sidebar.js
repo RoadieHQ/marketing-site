1c0e5b1556fe7989d486396aa92ff478ee1d1272
Brian Fletcher <brian@roadie.io>
2022-05-17 15:47:33 +0100

    adds scrolling for the table of contents

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,7 +4,7 @@ import classnames from 'classnames';
 const Sidebar = ({ children, className, side = 'left', sticky = false }) => (
   <aside
     className={
-      classnames('md:w-72 border-gray-100', {
+      classnames('md:w-72 border-gray-100', "overflow-y-scroll", {
         'md:border-r-2': side === 'left',
         'md:border-l-2': side === 'right',
       }, className)
