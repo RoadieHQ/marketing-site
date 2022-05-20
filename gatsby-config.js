68fc16297082d1e16262262b84eda756e9a1677c
David Tuite <david@roadie.io>
2022-05-20 15:36:28 +0100

    Less changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -182,7 +182,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-csp',
       options: {
-        disableOnDev: true,
+        disableOnDev: false,
         // This plugin does correctly add hashes to the 'style-src' directive. However, it doesn't
         // seem to identify all of the required hashes, so the unsafe-inline keyword is still
         // required. The 'unsafe-inline' keyword is ignored by browsers if any hashes are found,
