3e07529d835140e1257958a73e1c08b2b24d17f2
David Tuite <david@roadie.io>
2022-07-15 17:24:32 +0100

    Add Crystal testimonial

--- src/components/landing/Testimonials/SidebarTestimonial.js
+++ src/components/landing/Testimonials/SidebarTestimonial.js
@@ -8,7 +8,7 @@ import enriqueAvatarPng from '../../../../content/assets/home/testimonial/enriqu
 import contentfulLogo from '../../../../content/assets/home/testimonial/enrique-contentful/contentful-monochrome-white.webp';
 import contentfulLogoPng from '../../../../content/assets/home/testimonial/enrique-contentful/contentful-monochrome-white.png';
 
-const Testimonial = () => (
+const EnriqueTestimonial = () => (
   <div className="py-12 px-4 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 lg:pr-16">
     <div className="md:flex-shrink-0">
       <picture>
@@ -53,4 +53,49 @@ const Testimonial = () => (
   </div>
 );
 
-export default Testimonial;
+const CrystalTestimonial = () => (
+  <div className="py-12 px-4 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 lg:pr-16">
+    <div className="md:flex-shrink-0">
+      <picture>
+        <source srcSet={snykLogo} type="image/webp" />
+        <source srcSet={snykLogoPng} type="image/png" />
+
+        <img
+          src={snykLogoPng}
+          alt="Snyk logo"
+          height="33"
+          width="160"
+        />
+      </picture>
+    </div>
+
+    <blockquote className="mt-6 md:flex-grow md:flex md:flex-col">
+      <div className="relative text-lg font-medium text-white md:flex-grow">
+        <QuoteIcon />
+
+        <p className="relative">
+          Roadie are amazing. I totally recommend them. They’re great to work with and super responsive and helpful.
+        </p>
+      </div>
+
+      <footer className="mt-8">
+        <div className="flex items-start">
+          <Avatar
+            avatar={{
+              webp: crystalAvatar,
+              png: crystalAvatarPng,
+              alt: ``,
+            }}
+          />
+
+          <div className="ml-4">
+            <div className="text-base font-medium text-white">Crystal Hirschorn</div>
+            <div className="text-base font-medium text-primary-200">Staff Software Engineer</div>
+          </div>
+        </div>
+      </footer>
+    </blockquote>
+  </div>
+);
+
+export default EnriqueTestimonial;
