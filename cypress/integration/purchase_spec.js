00d3777dd6d23bf36a689a45fe58966a8c6429dd
David Tuite <david@roadie.io>
2022-08-05 08:10:28 +0100

    Fix pricing (#814)
    * Change exchange rate
    
    * Change currency exchange rate

--- cypress/integration/purchase_spec.js
+++ cypress/integration/purchase_spec.js
@@ -18,7 +18,7 @@ describe('Purchasing', () => {
     cy.contains('$900 billed monthly');
 
     cy.get('button#currency-switcher-eur').click();
-    cy.contains('€875 billed monthly');
+    cy.contains('€900 billed monthly');
 
     const PLAN_NAME = 'Roadie-Teams-Plan-EUR-Monthly';
     cy.get('a#teams-plan-cta').should('have.attr', 'href', `${CHARGEBEE_URL}?subscription_items%5Bitem_price_id%5D%5B0%5D=${PLAN_NAME}&subscription_items%5Bquantity%5D%5B0%5D=50`);
