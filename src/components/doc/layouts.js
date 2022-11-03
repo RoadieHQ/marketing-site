b3e5f182bb4ee02afbe1f08fa4374c573f1a0f44
Miklós Kiss <miklos@roadie.io>
2022-11-03 10:18:00 +0100

    add scaffolder-cookbook (#875)

--- src/components/doc/layouts.js
+++ src/components/doc/layouts.js
@@ -1,24 +1,34 @@
 import sidebar from '../../../content/docs/docs-nav.yaml';
 
-export const DOCS_LAYOUTS = [{
-  tabLabel: 'Home',
-  startPath: '/docs/',
-  isActiveMatch: /^\/docs\/$/,
-  sidebarNavItemGroups: null,
-}, {
-  tabLabel: 'Getting started',
-  startPath: '/docs/getting-started/install-github-app/',
-  isActiveMatch: /\/docs\/getting-started/,
-  sidebarNavItemGroups: [sidebar.nav[0]],
-}, {
-  tabLabel: 'Plugins & Integrations',
-  startPath: '/docs/integrations/',
-  isActiveMatch: '/docs/(?:custom-plugins|integrations)',
-  sidebarNavItemGroups: [sidebar.nav[1], sidebar.nav[2], sidebar.nav[3]],
-}, {
-  tabLabel: 'In-depth',
-  startPath: '/docs/details/github-app-permissions/',
-  isActiveMatch: '/docs/details',
-  sidebarNavItemGroups: [sidebar.nav[4]],
-}];
-
+export const DOCS_LAYOUTS = [
+  {
+    tabLabel: 'Home',
+    startPath: '/docs/',
+    isActiveMatch: /^\/docs\/$/,
+    sidebarNavItemGroups: null,
+  },
+  {
+    tabLabel: 'Getting started',
+    startPath: '/docs/getting-started/install-github-app/',
+    isActiveMatch: /\/docs\/getting-started/,
+    sidebarNavItemGroups: [sidebar.nav[0]],
+  },
+  {
+    tabLabel: 'Plugins & Integrations',
+    startPath: '/docs/integrations/',
+    isActiveMatch: '/docs/(?:custom-plugins|integrations)',
+    sidebarNavItemGroups: [sidebar.nav[1], sidebar.nav[2], sidebar.nav[3]],
+  },
+  {
+    tabLabel: 'In-depth',
+    startPath: '/docs/details/github-app-permissions/',
+    isActiveMatch: '/docs/details',
+    sidebarNavItemGroups: [sidebar.nav[4]],
+  },
+  {
+    tabLabel: 'Scaffolder Cookbook',
+    startPath: '/docs/scaffolder-cookbook/create-rfc',
+    isActiveMatch: /\/docs\/scaffolder-cookbook/,
+    sidebarNavItemGroups: [sidebar.nav[5]],
+  },
+];
