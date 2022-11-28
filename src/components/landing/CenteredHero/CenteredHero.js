2b49fa8e2acea66a43dd6b6dd318c6f4a76ce978
Jorge Lainfiesta <jorgelainfiesta@users.noreply.github.com>
2022-11-28 13:41:26 +0100

    Update logos (#887)
    * Update logos
    
    * Uses aligned logos

--- src/components/landing/CenteredHero/CenteredHero.js
+++ src/components/landing/CenteredHero/CenteredHero.js
@@ -6,7 +6,7 @@ import { LOGOS } from '../CustomerLogoCloud';
 import has from 'lodash/has';
 
 const CenteredHero = () => {
-  const whiteLogos = [...LOGOS].filter(({ src }) => has(src, 'white')).slice(0, 4);
+  const whiteLogos = [...LOGOS].filter(({ src }) => has(src, 'white')).slice(0, 5);
 
   return (
     <>
@@ -29,7 +29,7 @@ const CenteredHero = () => {
                 <div className="flex-wrap mt-5 md:mt-20 sm:col-span-6 sm:flex-nowrap flex center justify-between">
                   {whiteLogos.map((logo) => (
                     <img
-                      className="m-3 h-8 opacity-80"
+                      className="m-3 h-12 opacity-80"
                       key={`hero-logo-${logo.alt}`}
                       src={logo.src.white}
                       alt={logo.alt}
