6922c074f904493b9b56ced77ec000b339c68666
David Tuite <david@roadie.io>
2023-02-28 15:59:00 +0000

    Add pragmatic  engineer banner

---
+++ src/components/TopBanner/PragmaticEngineerNewsletter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../../content/assets/pragmatic-engineer-logo.png';
+import { Link } from 'components';
+
+const PragmaticEngineerNewsletter = () => (
+  <Link
+    to="/request-demo/"
+    className="text-white underline-none hover:underline"
+  >
+    <img
+      src={logo}
+      alt="The Pragmatic Engineer logo. Three orange columns ordered tallest to shortest."
+      className="h-4 mr-2 inline align-middle"
+      style={{ marginBottom: 3 }}
+    />
+    Welcome all Pragmatic Engineer readers! Click here to book a demo of Roadie Backstage.
+  </Link>
+);
+
+export default PragmaticEngineerNewsletter;
