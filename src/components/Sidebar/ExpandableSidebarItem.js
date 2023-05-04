88a8b31d92b18ab16661ede3b861d3049087198b
David Tuite <david@roadie.io>
2023-05-04 09:39:08 +0100

    Create expandable sidebar

---
+++ src/components/Sidebar/ExpandableSidebarItem.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { ChevronDownIcon, ChevronRightIcon } from '@heroicons/react/outline'
+import classnames from 'classnames';
+
+import SidebarItem from './Item';
+
+const ExpandableSidebarItem = ({ text, subItems, location }) => {
+  const isOpenOnPageLoad = subItems.map(Object.values).flat().includes(location.pathname);
+  const [isOpen, setIsOpen] = useState(isOpenOnPageLoad);
+  const subMenuButtonIcon = isOpen ?
+    <ChevronDownIcon className="h-4" /> :
+    <ChevronRightIcon className="h-4" />;
+
+  const toggleSubMenuOpen = () => {
+    if (isOpen) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+  };
+
+  const itemComponents = subItems.map((item) => {
+    const [[text, path]] = Object.entries(item);
+
+    return <SidebarItem key={path} text={text} to={path} className="pl-10" />;
+  });
+
+  return (
+    <li>
+      <div>
+        <button onClick={toggleSubMenuOpen} className="flex items-center">
+          <span className="mr-1">{subMenuButtonIcon}</span>
+          <span>{text}</span>
+        </button>
+      </div>
+      <div className={classnames({ 'h-0 hidden': !isOpen, 'h-full': isOpen })}>
+        <ul className="p-0 list-none">
+          {itemComponents}
+        </ul>
+      </div>
+    </li>
+  );
+};
+
+export default ExpandableSidebarItem;
