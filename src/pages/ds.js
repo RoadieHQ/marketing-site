1c40d8c8d231fb628015913a93cb6434460c8b0b
colmtuite <colm@workos.com>
2023-07-27 15:55:35 +0100

    More scales. Badge component.

--- src/pages/ds.js
+++ src/pages/ds.js
@@ -9,8 +9,8 @@ const DS = () => (
     />
     <div className='Container'>
       <section className='Section size-3'>
-        <div className='Flex column gap-4'>
-          <div className='Flex row gap-4'>
+        <div className='Flex column gap-5'>
+          <div className='Flex row gap-5'>
             <button className='Button size-1 neutral'>
               <svg width="16" height="16" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
               Add Field
@@ -24,9 +24,41 @@ const DS = () => (
               Request a Demo
             </button>
           </div>
-          <div className='Flex row gap-4'>
+          <div className='Flex row gap-5'>
             <button className='Button size-2 neutral'>Request a Demo</button>
             <button className='Button size-2 accent'>Request a Demo</button>
+            <button className='Button size-2 green'>Request a Demo</button>
+            <button className='Button size-2 red'>Request a Demo</button>
+          </div>
+        </div>
+      </section>
+
+      <section className='Section size-3'>
+        <div className='Flex column gap-4'>
+          <div className='Flex row gap-4'>
+            <span className='Badge size-1 gray'>
+              Coming Soon
+            </span>
+            <span className='Badge size-2 gray'>
+              Coming Soon
+            </span>
+          </div>
+          <div className='Flex row gap-4'>
+            <span className='Badge size-2 gray'>
+              Coming Soon
+            </span>
+            <span className='Badge size-2 indigo'>
+              Owner
+            </span>
+            <span className='Badge size-2 green'>
+              Approved
+            </span>
+            <span className='Badge size-2 yellow'>
+              Pending
+            </span>
+            <span className='Badge size-2 red'>
+              Declined
+            </span>
           </div>
         </div>
       </section>
@@ -103,7 +135,7 @@ const DS = () => (
           <div className='Flex column gap-6'>
             <h1 className='Text size-9'>SaaS Backstage. Simpler, safer, and more powerful.</h1>
             <p className='Text size-5 weight-1 lowContrast'>Get automatic upgrades, instant Catalog updates, and ready-to-use integrations with Roadie’s Backstage-as-a-Service.</p>
-            <button className='Button size-2 accent'>Request a Demo</button>
+            <button className='Button size-3 accent'>Request a Demo</button>
           </div>
           <div>
             <div style={{ backgroundColor: 'gainsboro', height: 500 }}></div>
