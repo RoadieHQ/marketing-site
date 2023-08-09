e00af9a5812b2a3889adc865bf434aa208833f3a
David Tuite <david@roadie.io>
2023-08-09 15:31:53 +0100

    Lint improvements

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,7 @@ module.exports = {
             },
           },
           {
-            resolve: "gatsby-remark-embed-video",
+            resolve: 'gatsby-remark-embed-video',
             options: {
               width: 800,
               ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
@@ -128,12 +128,11 @@ module.exports = {
               loadingStrategy: 'lazy', //Optional: Enable support for lazy-load offscreen iframes. Default is disabled.
               urlOverrides: [
                 {
-                  id: "youtube",
-                  embedURL: videoId =>
-                      `https://www.youtube-nocookie.com/embed/${videoId}`,
+                  id: 'youtube',
+                  embedURL: (videoId) => `https://www.youtube-nocookie.com/embed/${videoId}`,
                 },
               ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
-              containerClass: "embedVideo-container", //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
+              containerClass: 'embedVideo-container', //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
               iframeId: false, //Optional: if true, iframe's id will be set to what is provided after 'video:' (YouTube IFrame player API requires iframe id)
             },
           },
