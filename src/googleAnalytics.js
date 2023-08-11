b7b9636e9340dbedc55cccdef6725b7ca2487300
Jorge Lainfiesta <jorge@roadie.io>
2023-08-11 16:38:00 +0200

    Correct tracking functions

--- src/googleAnalytics.js
+++ src/googleAnalytics.js
@@ -2,6 +2,6 @@ const trackGoogleAnalyticsEvent = (eventName, eventParams) => {
   return typeof window !== "undefined" && window.gtag("event", eventName, eventParams);
 };
 
-export const trackRequestDemo = trackGoogleAnalyticsEvent("request_demo");
-export const trackRequestTrial = trackGoogleAnalyticsEvent("request_trial");
-export const trackSubscribe = trackGoogleAnalyticsEvent("subscribe_newsletter");
+export const trackRequestDemo = () => trackGoogleAnalyticsEvent("request_demo");
+export const trackRequestTrial = () => trackGoogleAnalyticsEvent("request_trial");
+export const trackSubscribe = () => trackGoogleAnalyticsEvent("subscribe_newsletter");
