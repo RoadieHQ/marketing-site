c2f1610b65f0a3f0f47bd41941106ac182bca08d
sblausten <sam@roadie.io>
2023-12-27 13:39:46 +0100

    Remove api call

--- bin/fetch_api_docs.sh
+++ bin/fetch_api_docs.sh
@@ -5,4 +5,3 @@ set -e
 curl -v https://api.roadie.so/api/scaffolder/api-docs -H "Authorization: Bearer $ROADIE_API_TOKEN" > static/scaffolder-openapi.json
 curl -v https://api.roadie.so/api/catalog/api-docs -H "Authorization: Bearer $ROADIE_API_TOKEN" > static/catalog-openapi.json
 curl -v https://api.roadie.so/api/tech-insights/v1/api-docs -H "Authorization: Bearer $ROADIE_API_TOKEN" > static/tech-insights-openapi.json
-curl -v https://api.roadie.so/api/plugins/api-docs -H "Authorization: Bearer $ROADIE_API_TOKEN" > static/plugins-openapi.json
