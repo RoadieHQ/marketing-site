ae6c03c15cf6c9a9998f02ba30364e8954464646
roadgork <155967328+roadgork@users.noreply.github.com>
2024-02-20 10:31:47 +0000

    Update integrations.js

--- src/pages/docs/integrations.js
+++ src/pages/docs/integrations.js
@@ -140,15 +140,14 @@ const DocsIntegrations = ({
     if (integrationTypeFilter === INTEGRATION_TYPE_FILTERS.ALL) {
       filteredIntegrations = [...allIntegrations];
     } else {
-      filteredIntegrations = allIntegrations.filter(({ frontmatter }) => {
-        if (!frontmatter.integrationType) return false;
-        return frontmatter.integrationType.toLowerCase() === integrationTypeFilter;
-      });
+      filteredIntegrations = allIntegrations.filter(
+        ({ frontmatter }) => frontmatter?.integrationType?.toLowerCase() === integrationTypeFilter
+      );
     }
   } else {
-    filteredIntegrations = allIntegrations.filter(({ frontmatter }) => {
-      return frontmatter.humanName.toLowerCase().includes(query.toLowerCase());
-    });
+    filteredIntegrations = allIntegrations.filter(({ frontmatter }) =>
+      frontmatter?.humanName?.toLowerCase().includes(query.toLowerCase())
+    );
   }
 
   const handleQueryInput = (queryValue) => {
