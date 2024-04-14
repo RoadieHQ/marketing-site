fa573b0b947c5fc4453d794bcae5355671eb881c
irma12 <irma@roadie.io>
2024-04-14 21:40:48 +0200

    Add higlighting and handle cases with multiple chips

---
+++ content/docs/getting-started/adding-a-catalog-item/github copy/index.md
@@ -0,0 +1,83 @@
+---
+title: GitHub1111
+publishedDate: '2022-04-04T14:00:00.0Z'
+description: How to configure the GitHub to create a catalog item.
+
+humanName: GitHub10
+examples:
+  - name: componento
+    language: yaml
+    content: |
+      apiVersion: backstage.io/v1alpha1
+      kind: Component
+      metadata:
+        name: artist-web
+        description: The place to be, for great artists
+      spec:
+        type: website
+        lifecycle: production
+        owner: artist-relations-team
+        system: artist-engagement-portal
+        dependsOn:
+          - resource:default/artists-db
+        providesApis:
+          - artist-api
+  - name: resource
+    language: yaml
+    content: |
+      apiVersion: backstage.io/v1alpha1
+      kind: Resource
+      metadata:
+        name: artists-db
+        description: Stores artist details
+      spec:
+        type: database
+        owner: artist-relations-team
+        system: artist-engagement-portal
+  - name: api
+    language: yaml
+    content: |
+      apiVersion: backstage.io/v1alpha1
+      kind: API
+      metadata:
+        name: artist-api
+        description: Retrieve artist details
+      spec:
+        type: openapi
+        lifecycle: production
+        owner: artist-relations-team
+        system: artist-engagement-portal
+        definition: |
+          openapi: "3.0.0"
+          info:
+            version: 1.0.0
+            title: Artist API
+            license:
+              name: MIT
+          servers:
+            - url: http://artist.spotify.net/v1
+          paths:
+            /artists:
+              get:
+                summary: List all artists
+---
+
+### Install the GitHub App
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+### Use a personal access token
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+### Upload a catalog item to a repository
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+# Import the catalog item into Roadie
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+# Enable auto discovery
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatu
--- src/pages/docs/getting-started/addcatalogitem.js
+++ src/pages/docs/getting-started/addcatalogitem.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 import { CodeBlock, Headline, SitewideFooter } from 'components';
 import classnames from 'classnames';
+import { Popover, Transition } from '@headlessui/react';
 import Sidebar from 'components/doc/Sidebar';
 import isEmpty from 'lodash/isEmpty';
 import { graphql } from 'gatsby';
+import Prism from 'prismjs';
 
 import DocsHeader from 'components/SitewideHeader/DocsHeader';
 
@@ -15,51 +17,255 @@ const Chip = ({ label, isActive }) => {
   if (isActive) colorClasses = 'bg-primary-100 text-primary-800';
   if (!isActive) colorClasses = 'text-gray-800 border-2';
 
-  return <span className={classnames(defaultClasses, colorClasses)}>{label}</span>;
+  return <span className={classnames('capitalize', defaultClasses, colorClasses)}>{label}</span>;
+};
+
+const ShowMore = ({ item, itemName, onClick, sources }) => {
+  return (
+    <button onClick={() => onClick(item, sources)}>
+      <Chip label={itemName} />
+    </button>
+  );
+};
+
+const DefaultChip = ({ displayedChips, onClick, typeFilter, sources }) => {
+  const defaultClasses =
+    'inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium mr-2';
+
+  if (isEmpty(displayedChips)) return null;
+
+  return (
+    <>
+      <Popover className="relative">
+        {() => (
+          <>
+            <Popover.Button className={classnames(defaultClasses, 'text-gray-800 border-2')}>
+              <span>Show more</span>
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute z-50 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen sm:max-w-xs sm:px-0">
+                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                  <div className="relative grid gap-3 bg-white px-5 py-6 sm:gap-4 sm:p-4">
+                    {displayedChips.map((item) => (
+                      <ShowMore
+                        item={item}
+                        itemName={item.frontmatter.humanName}
+                        sources={sources}
+                        key={item.name}
+                        typeFilter={typeFilter}
+                        onClick={onClick}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </>
+        )}
+      </Popover>
+    </>
+  );
+};
+
+const DefaultKindChip = ({ displayedChips, onClick, typeFilter, sources }) => {
+  const defaultClasses =
+    'inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium mr-2';
+
+  if (isEmpty(displayedChips)) return null;
+
+  return (
+    <>
+      <Popover className="relative">
+        {() => (
+          <>
+            <Popover.Button className={classnames(defaultClasses, 'text-gray-800 border-2')}>
+              <span>Show more</span>
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute z-50 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen sm:max-w-xs sm:px-0">
+                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                  <div className="relative grid gap-3 bg-white px-5 py-6 sm:gap-4 sm:p-4">
+                    {displayedChips.map((item) => (
+                      <ShowMore
+                        item={item}
+                        itemName={item.name}
+                        sources={sources}
+                        key={item.name}
+                        typeFilter={typeFilter}
+                        onClick={onClick}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </>
+        )}
+      </Popover>
+    </>
+  );
 };
 
 const Sources = ({ sources }) => {
   const [typeFilter, setTypeFilter] = useState(sources[0].id);
-  const [allKinds, setAllKinds] = useState(sources[0].frontmatter.examples);
+  const [allKinds, setAllKinds] = useState(
+    sources.find((item) => item.id === typeFilter).frontmatter.examples
+  );
   const [kindFilter, setKind] = useState(sources[0].frontmatter.examples[0].name);
+  const [showAllChips, setShowAllChips] = useState(false);
 
+  const [displayedChips, setDisplayedChips] = useState(sources.slice(0, 4));
+  const [hiddenChips, setHiddenChips] = useState(sources.slice(5, sources.length));
+
+  const [displayedKindChips, setDisplayedKindChips] = useState(allKinds.slice(0, 4));
+
+  const [hiddenKindChips, setHiddenKindChips] = useState(allKinds.slice(5, allKinds.length));
+  useEffect(() => {
+    Prism.highlightAll();
+  });
   if (isEmpty(sources)) return null;
 
   const handleChange = (it) => {
     setTypeFilter(it.id);
     setAllKinds(it.frontmatter.examples);
+    setKind(sources.find((item) => item.id === it.id).frontmatter.examples[0].name);
+    setDisplayedKindChips(
+      sources.find((item) => item.id === it.id).frontmatter.examples.slice(0, 4)
+    );
+    setHiddenKindChips(
+      sources
+        .find((item) => item.id === it.id)
+        .frontmatter.examples.slice(
+          5,
+          sources.find((item) => item.id === it.id).frontmatter.examples.length
+        )
+    );
+  };
+
+  const handleShowMoreClick = () => {
+    setShowAllChips(!showAllChips);
+  };
+
+  const handleItemClick = (
+    item,
+    displayedChips,
+    hiddenChips,
+    setDisplayedChips,
+    setHiddenChips,
+    extraAction
+  ) => {
+    const index = hiddenChips.indexOf(item);
+
+    // Make a copy of displayedChips and hiddenChips arrays
+    const newDisplayedChips = [...displayedChips];
+    const newHiddenChips = [...hiddenChips];
+
+    // Remove the selected item from newHiddenChips
+    newHiddenChips.splice(index, 1);
+
+    // Push the last item to newHiddenChips
+    newHiddenChips.push(newDisplayedChips.pop());
+
+    // Push the selected item to newDisplayedChips
+    newDisplayedChips.push(hiddenChips[index]);
+
+    // // Update state with the new arrays
+    setDisplayedChips(newDisplayedChips);
+    setHiddenChips(newHiddenChips);
+
+    if (extraAction) {
+      extraAction(item);
+    }
+
+    setShowAllChips(false);
+  };
+
+  const handleShowItemClick = (item) => {
+    handleItemClick(
+      item,
+      displayedChips,
+      hiddenChips,
+      setDisplayedChips,
+      setHiddenChips,
+      handleChange
+    );
+  };
+
+  const handleShowKindClick = (item) => {
+    handleItemClick(
+      item,
+      displayedKindChips,
+      hiddenKindChips,
+      setDisplayedKindChips,
+      setHiddenKindChips,
+      (item) => setKind(item.name)
+    );
   };
 
   return (
-    <section className="sm:grid sm:grid-cols-3 sm:gap-6">
+    <section className="sm:grid sm:grid-cols-2 sm:gap-6">
       <div>
         <div className="mb-6 mt-6">
           <span className="mr-4">Source: </span>
-          {sources.map((it) => {
+          {displayedChips.map((it) => {
             return (
               <button key={it.id} onClick={() => handleChange(it)}>
                 <Chip label={it.frontmatter.humanName} isActive={typeFilter === it.id} />
               </button>
             );
           })}
+          {sources.length > 5 && (
+            <button className="show-more" onClick={handleShowMoreClick}>
+              <DefaultChip
+                displayedChips={hiddenChips}
+                onClick={handleShowItemClick}
+                typeFilter={typeFilter}
+                sources={sources}
+              />
+            </button>
+          )}
         </div>
-
-          <CodeBlock
-            language='html'
-            intro={sources.find((it) => it.id === typeFilter).html}
-          />
+        <CodeBlock language="html" intro={sources.find((it) => it.id === typeFilter).html} />
       </div>
       <div>
         <div className="mb-6 mt-6">
           <span className="mr-4">Kind: </span>
           {!isEmpty(allKinds) &&
-            allKinds.map((k) => {
+            displayedKindChips.map((k) => {
               return (
                 <button key={k.name} onClick={() => setKind(k.name)}>
                   <Chip label={k.name} isActive={kindFilter === k.name} />
                 </button>
               );
             })}
+          {allKinds.length > 5 && (
+            <button className="show-more" onClick={handleShowMoreClick}>
+              <DefaultKindChip
+                displayedChips={hiddenKindChips}
+                onClick={handleShowKindClick}
+                typeFilter={typeFilter}
+                sources={allKinds}
+              />
+            </button>
+          )}
         </div>
         <div className="mb-6">
           <CodeBlock
