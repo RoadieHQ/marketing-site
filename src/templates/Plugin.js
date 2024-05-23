fcb8310353041ff211fc9dccae4e2beacb270914
David Tuite <david@roadie.io>
2024-05-23 14:16:19 +0100

    Reapply "Add exit intent popup to plugin directory (#1298)" (#1305)
    This reverts commit 38e7a4113e909695280e3c8f3fd8144cc7fc3d59.

--- src/templates/Plugin.js
+++ src/templates/Plugin.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import usePageLeave from 'react-use/lib/usePageLeave';
 import Prism from 'prismjs';
 import { graphql } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
-import { Button, Title, CodeBlock, SEO, SitewideHeader, SitewideFooter } from 'components';
+import { Button, Title, CodeBlock, SEO, SitewideHeader, SitewideFooter, ExitIntentModal } from 'components';
 import { EditOnGitHubLink, Header } from 'components/backstage/plugins';
 import {
   SubscribeToNewsletterSuccessModal,
@@ -58,14 +59,23 @@ const PluginTemplate = ({ data }) => {
 
   const [email, setEmail] = useState('');
   const [modalOpen, setModalOpen] = useState(false);
+  const [exitIntentModalOpen, setExitIntentModalOpen] = useState(false);
 
   const handleCloseModal = () => {
     setModalOpen(false);
     setEmail('');
   };
 
+  const handleCloseExitIntentModal = () => {
+    setExitIntentModalOpen(false);
+  };
+
   useEffect(() => {
     Prism.highlightAll();
+  });
+
+  usePageLeave(() => {
+    setExitIntentModalOpen(true);
   });
 
   return (
@@ -77,6 +87,11 @@ const PluginTemplate = ({ data }) => {
         handleCloseModal={handleCloseModal}
         siteMetadata={data.site.siteMetadata}
         email={email}
+      />
+
+      <ExitIntentModal
+        modalOpen={exitIntentModalOpen}
+        handleCloseModal={handleCloseExitIntentModal}
       />
 
       <SitewideHeader />
