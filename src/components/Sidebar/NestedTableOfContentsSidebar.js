d0543dd4b8617ea9499b2681b9526def4ab52b42
David Tuite <david@roadie.io>
2024-08-31 15:47:20 +0100

    Allow ToC sidebar to scrol internally

--- src/components/Sidebar/NestedTableOfContentsSidebar.js
+++ src/components/Sidebar/NestedTableOfContentsSidebar.js
@@ -8,6 +8,12 @@ const numberOfContentsItems = (tableOfContents) => (
   (tableOfContents.match(/<li>/g) || []).length
 );
 
+// This allows the sidebar to scroll without scrolling the whole page. No idea how it works.
+// https://stackoverflow.com/a/13337664/574190
+const sidebarInternalHeight = {
+  height: 'calc(100vh - 9rem)',
+};
+
 const NestedTableOfContentsSidebar = ({ tableOfContents, className }) => {
   // There's no benefit to the ToC if it's very short.
   if (numberOfContentsItems(tableOfContents) < 3) {
@@ -24,13 +30,13 @@ const NestedTableOfContentsSidebar = ({ tableOfContents, className }) => {
         <strong>Table of Contents</strong>
       </SidebarSection>
 
-      <SidebarSection>
+      <div className="mb-7 pl-3 overflow-y-scroll" style={sidebarInternalHeight}>
         {/* There is a CSS stylesheet that targets this class name */}
         <div
           className="table-of-contents-sidebar"
           dangerouslySetInnerHTML={{ __html: tableOfContents }}
         />
-      </SidebarSection>
+      </div>
     </Sidebar>
   );
 };
