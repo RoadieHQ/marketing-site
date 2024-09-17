e350596ccf551d05bfdcc1cddf9a2b5346501175
David Tuite <david@roadie.io>
2024-09-17 07:45:14 +0100

    Fix links (#1387)
    * Fix 404s
    
    * Combine robots config and sitemap config
    
    * Delete empty docs page
    
    * Disallow partial pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,28 @@ const getContentfulOptions = () => {
   };
 };
 
+// These paths are blocked from search engine indexing and search engine access.
+const DISLALLOW_LIST = [
+  '/purchase/',
+  '/purchase/success/',
+  '/tailwind/404/',
+  '/installation-pending/',
+
+  // These are partials which eventually get embedded into other pages.
+  '/docs/details/create-proxy/structure/header/',
+  '/docs/getting-started/adding-a-catalog-item/structure/header/',
+  '/docs/details/create-proxy/basic/',
+
+  // These are partials which eventually get embedded into other pages.
+  '/docs/getting-started/adding-a-catalog-item/bitbucket/',
+  '/docs/getting-started/adding-a-catalog-item/github/',
+  '/docs/getting-started/adding-a-catalog-item/gitlab/',
+  '/docs/getting-started/adding-a-catalog-item/roadie-api/',
+  '/docs/getting-started/adding-a-catalog-item/aws-s3/',
+  '/docs/getting-started/adding-a-catalog-item/azure-devops/',
+  '/docs/getting-started/adding-a-catalog-item/roadie-cli/',
+];
+
 // Only environment variables prefixed with GATSBY_ are available in the runtime. Here we turn
 // a server side variable into a runtime one. This variable is later used to determine which
 // branch of a split testing experiment we are on.
@@ -187,7 +209,21 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-sitemap',
       options: {
-        excludes: ['/purchase/', '/purchase/success/', '/tailwind/404/'],
+        excludes: DISLALLOW_LIST,
+      },
+    },
+
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: getSiteUrl(),
+        sitemap: `${getSiteUrl()}/sitemap-index.xml`,
+        policy: [
+          {
+            userAgent: '*',
+            disallow: DISLALLOW_LIST,
+          },
+        ],
       },
     },
 
