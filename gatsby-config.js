56256a4597ed2fdcde839a17f665905030fb4efd
Sam Nixon <samnixon87@gmail.com>
2024-09-19 13:48:46 +0200

    Merge latest changes from main

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,28 @@ const getContentfulOptions = () => {
   };
 };
 
+// These paths are blocked from search engine indexing and search engine access.
+const DISLALLOW_LIST = [
+  '/purchase/',
+  '/purchase/success/',
+  '/tailwind/404/',
+  '/installation-pending/',
+
+  // These are partials which eventually get embedded into other pages.
+  '/docs/details/create-proxy/structure/header/',
+  '/docs/getting-started/adding-a-catalog-item/structure/header/',
+  '/docs/details/create-proxy/basic/',
+
+  // These are partials which eventually get embedded into other pages.
+  '/docs/getting-started/adding-a-catalog-item/bitbucket/',
+  '/docs/getting-started/adding-a-catalog-item/github/',
+  '/docs/getting-started/adding-a-catalog-item/gitlab/',
+  '/docs/getting-started/adding-a-catalog-item/roadie-api/',
+  '/docs/getting-started/adding-a-catalog-item/aws-s3/',
+  '/docs/getting-started/adding-a-catalog-item/azure-devops/',
+  '/docs/getting-started/adding-a-catalog-item/roadie-cli/',
+];
+
 // Only environment variables prefixed with GATSBY_ are available in the runtime. Here we turn
 // a server side variable into a runtime one. This variable is later used to determine which
 // branch of a split testing experiment we are on.
@@ -89,24 +111,7 @@ module.exports = {
         ignore: ['**/.*', '**/template.md'],
       },
     },
-    {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {
-        output: `/sitemap-images`,
-        serialize: ({ path, allFile }) => {
-          const images = allFile.nodes.map((node) => ({
-            url: `https://roadie.io${node.publicURL}`,
-          }));
 
-          return {
-            url: path,
-            changefreq: `daily`,
-            priority: 0.7,
-            images,
-          };
-        },
-      },
-    },
     {
       resolve: `gatsby-plugin-algolia`,
       options: {
@@ -201,7 +206,53 @@ module.exports = {
       },
     },
 
-    'gatsby-plugin-advanced-sitemap',
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        output: `/sitemap-index`,
+        excludes: DISLALLOW_LIST,
+        query: `
+          {
+            allSitePage {
+              nodes {
+                path
+              }
+            }
+            allFile(filter: { extension: { regex: "/(jpg|jpeg|png|webp)/" } }) {
+              nodes {
+                publicURL
+              }
+            }
+          }
+        `,
+        serialize: ({ path, allFile }) => {
+          const images = allFile.nodes.map((node) => ({
+            url: `https://roadie.io${node.publicURL}`,
+          }));
+
+          return {
+            url: path,
+            changefreq: `daily`,
+            priority: 0.7,
+            images,
+          };
+        },
+      },
+    },
+
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: getSiteUrl(),
+        sitemap: `${getSiteUrl()}/sitemap-index.xml`,
+        policy: [
+          {
+            userAgent: '*',
+            disallow: DISLALLOW_LIST,
+          },
+        ],
+      },
+    },
 
     {
       resolve: 'gatsby-plugin-react-svg',
