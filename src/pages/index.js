2ee8d37b0b631e78e94ac9e44be14f343e2b4f00
Brian Fletcher <brian@roadie.io>
2024-10-04 16:39:48 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ import ArgoLogo from '../../content/assets/logos/argo-cd/argo.inline.svg';
 import JiraSoftwareLogo from '../../content/assets/logos/jira/jira.inline.svg';
 
 const SEO_TITLE = 'Roadie - Internal Developer Portal built on Backstage';
-const LEAD = `Roadie is the most customizable managed Internal Developer Portal with built-in best practices, automated workflows, actionable insights built on Backstage`;
+const LEAD = `Roadie is the most customizable Internal Developer Portal with built-in best practices, automated workflows, actionable insights built on Backstage`;
 
 const PRODUCT = {
   features: [
