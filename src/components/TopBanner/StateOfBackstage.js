cd3e3ae5efe3c0d1ff8ef13d22e0e2c08eab58f4
David Tuite <david@roadie.io>
2025-03-10 17:30:55 +0000

    Change TopBanner to State of Backstage survey (#1479)
    * Change TopBanner to State of Backstage survey
    
    * Use https

---
+++ src/components/TopBanner/StateOfBackstage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'components';
+
+const StateOfBackstage = () => (
+  <Link
+    to="https://stateofbackstage.io"
+    className="text-white font-bold underline-none hover:underline"
+  >
+    Fill out the <span className="underline">State of Backstage survey</span> for a chance to win tickets to{' '}
+    <span className="uppercase bg-white text-blueroadie px-2 rounded-full mr-2">KUBECON EU 2025</span>
+  </Link>
+);
+
+export default StateOfBackstage;
