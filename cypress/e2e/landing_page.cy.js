d4c3ba75ae9963d65949ea423c5686ccee067bc0
Sam Nixon <samnixon87@gmail.com>
2025-03-31 17:27:59 +0200

    error, changing tests

--- cypress/e2e/landing_page.cy.js
+++ cypress/e2e/landing_page.cy.js
@@ -14,10 +14,7 @@ describe('The landing page', () => {
     cy.get('#request-demo-email-input').type('test@example.com');
     cy.get('#reported-attribution').type('Newsletter');
     cy.get('button[data-testid="sub-to-newsletter"]').click();
-    cy.get('button')
-      .contains('Request a demo')
-      .should('not.be.disabled', { timeout: 10000 }) // increase timeout to 10s, falls over at ~<4s
-      .click();
+    cy.get('button').contains('Request a demo').click();
     cy.contains("We'll be in touch");
   });
 
@@ -28,10 +25,7 @@ describe('The landing page', () => {
     cy.get('#request-demo-name-input').type('Mary Mac');
     cy.get('#request-demo-email-input').type('test@example.com');
     cy.get('#scm').select('Other');
-    cy.get('button')
-      .contains('Request a demo')
-      .should('not.be.disabled', { timeout: 10000 }) // increase timeout to 10s, falls over at ~<4s
-      .click();
+    cy.contains('Request a demo').click();
     cy.contains(`be in touch to learn more`);
   });
 });
