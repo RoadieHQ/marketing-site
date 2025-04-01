caecac9f8daa9e254ec831200f126c347edba5fd
Sam Nixon <samnixon87@gmail.com>
2025-04-01 09:55:03 +0200

    fix test

--- cypress/e2e/request_roadie_local.cy.js
+++ cypress/e2e/request_roadie_local.cy.js
@@ -10,28 +10,22 @@ describe('The Roadie Local page', () => {
 
   it('has a flow for requesting access to Roadie Local', () => {
     cy.visit(PAGE_PATHS.getRoadieLocal);
-    cy.contains('Request access').click();
 
     cy.get('#request-roadie-local-name-input').type('Mary Mac');
     cy.get('#request-roadie-local-email-input').type('test@example.com');
     cy.get('#reported-attribution').type('Newsletter');
     cy.get('button[data-testid="sub-to-newsletter"]').click();
-    cy.contains('Request access')
-      .should('not.be.disabled', { timeout: 10000 }) // wait up to 10s
-      .click();
+    cy.contains('button', 'Request access', { timeout: 10000 }).should('not.be.disabled').click();
     cy.contains("We'll be in touch");
   });
 
   it('has a flow for users who use choose other', () => {
     cy.visit(PAGE_PATHS.getRoadieLocal);
-    cy.contains('Request access').click();
 
     cy.get('#request-roadie-local-name-input').type('Mary Mac');
     cy.get('#request-roadie-local-email-input').type('test@example.com');
     cy.get('#scm').select('Other');
-    cy.contains('Request access')
-      .should('not.be.disabled', { timeout: 10000 }) // wait up to 10s
-      .click();
+    cy.contains('button', 'Request access', { timeout: 10000 }).should('not.be.disabled').click();
     cy.contains(`be in touch to learn more`);
   });
 });
