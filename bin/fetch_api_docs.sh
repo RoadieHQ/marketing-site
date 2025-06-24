8b87b837767ed34c201bbe233ffb0bbb0b73c11a
punkle <brian@roadie.io>
2025-06-24 14:25:46 +0100

    update roadie local binaries

--- bin/fetch_api_docs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-curl -v https://api.roadie.so/api/scaffolder/api-docs -H "Authorization: Bearer $ROADIE_API_TOKEN" > static/scaffolder-openapi.json
-curl -v https://api.roadie.so/api/catalog/api-docs -H "Authorization: Bearer $ROADIE_API_TOKEN" > static/catalog-openapi.json
-curl -v https://api.roadie.so/api/tech-insights/v1/api-docs -H "Authorization: Bearer $ROADIE_API_TOKEN" > static/tech-insights-openapi.json
---
+++ bin/fetch_api_docs_and_roadie_local.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+curl -v https://api.roadie.so/api/scaffolder/api-docs -H "Authorization: Bearer $ROADIE_API_TOKEN" > static/scaffolder-openapi.json
+curl -v https://api.roadie.so/api/catalog/api-docs -H "Authorization: Bearer $ROADIE_API_TOKEN" > static/catalog-openapi.json
+curl -v https://api.roadie.so/api/tech-insights/v1/api-docs -H "Authorization: Bearer $ROADIE_API_TOKEN" > static/tech-insights-openapi.json
+
+# Download Roadie Local CLI binaries
+VERSION="v0.1.1"
+DEST_DIR="static/downloads/roadie-local/v0.0.1"
+BASE_URL="https://github.com/RoadieHQ/roadie-local/releases/download/cli-${VERSION}"
+
+FILES=(
+  "roadie-local-cli-${VERSION}-darwin-amd64"
+  "roadie-local-cli-${VERSION}-darwin-arm64"
+  "roadie-local-cli-${VERSION}-linux-amd64"
+  "roadie-local-cli-${VERSION}-linux-arm64"
+)
+
+mkdir -p "${DEST_DIR}"
+
+for FILE in "${FILES[@]}"; do
+  echo "Downloading ${FILE}..."
+  curl -H "Authorization: token ${GITHUB_TOKEN}" -L -o "${DEST_DIR}/${FILE}" "${BASE_URL}/${FILE}"
+done
