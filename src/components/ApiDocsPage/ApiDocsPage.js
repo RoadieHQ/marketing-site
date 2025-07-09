1c73910e562ec9d2715e10dec918070975bd1820
irma12 <irma@roadie.io>
2025-07-09 10:29:44 +0200

    Just a small test

--- src/components/ApiDocsPage/ApiDocsPage.js
+++ src/components/ApiDocsPage/ApiDocsPage.js
@@ -8,19 +8,14 @@ import 'swagger-ui-react/swagger-ui.css';
 
 const ApiDocsPage = ({ location, url }) => (
   <>
-    <SEO
-      title={`API`}
-      description='Documentation of the Roadie API'
-    />
+    <SEO title={`API`} description="Documentation of the Roadie APIs" />
     <DocsHeader location={location} />
 
     <main className="md:flex pt-4 md:pt-0">
       <Sidebar location={location} />
 
       <article className="px-2 md:px-6 md:pt-7 md:flex-1">
-        <section
-          className="prose prose-primary max-w-full"
-        >
+        <section className="prose prose-primary max-w-full">
           <SwaggerUI url={url} />
         </section>
       </article>
