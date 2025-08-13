3e4b15866a0ac34f85d6f60439944b5f90994049
David Tuite <david@roadie.io>
2025-08-13 18:14:27 +0100

    Try logging

--- cypress/e2e/pricing.cy.js
+++ cypress/e2e/pricing.cy.js
@@ -4,9 +4,15 @@ describe('The pricing page', () => {
   it('should change currency', () => {
     cy.visit('/pricing/');
 
+    cy.get('#teams-cost-display').then((ele) => {
+      cy.log(ele.text());
+    });
     cy.get('#teams-cost-display')
       .contains(`$${Math.round(TIERS.teams.usdCentCostPerDevPerMonth / 100)}`);
     cy.get('#currency-switcher-eur').click();
+    cy.get('#teams-cost-display').then((ele) => {
+      cy.log(ele.text());
+    });
     cy.get('#teams-cost-display')
       .contains(`€${Math.round(TIERS.teams.eurCentCostPerDevPerMonth / 100)}`);
   });
