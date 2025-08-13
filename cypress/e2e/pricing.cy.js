6fbbd649015a8f43f56b27d929b70f9b50ccc8b3
David Tuite <david@roadie.io>
2025-08-13 14:10:58 +0100

    Add test

---
+++ cypress/e2e/pricing.cy.js
@@ -0,0 +1,11 @@
+import { TIERS } from '../../src/components/pricing/prices';
+
+describe('The pricing page', () => {
+  it('should change currency', () => {
+    cy.visit('/pricing/');
+
+    cy.get('#cost-display').contains(`$${Math.round(TIERS.teams.usdCentCostPerDevPerMonth / 100)}`);
+    cy.get('#currency-switcher-eur').click();
+    cy.get('#cost-display').contains(`€${Math.round(TIERS.teams.eurCentCostPerDevPerMonth / 100)}`);
+  });
+});
