9b1af3f802dcf29cdf93270bfc104531e4b7827e
David Tuite <david@roadie.io>
2025-08-14 15:41:03 +0100

    Remove enterprise

--- src/pages/pricing/index.js
+++ src/pages/pricing/index.js
@@ -41,7 +41,7 @@ const Pricing = ({
             />
           </div>
 
-          <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-3 sm:gap-6">
+          <div className={`mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-${TIERS.length} sm:gap-6 lg:max-w-4xl lg:mx-auto`}>
             {TIERS.map((tier) => (
               <Tier tier={tier} currentlySetCurrency={currentlySetCurrency} key={tier.name} />
             ))}
