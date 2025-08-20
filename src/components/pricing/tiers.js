7b3aafc7a0cc723f2f8f2bd03684303033e5ecc4
David Tuite <david@roadie.io>
2025-08-20 15:30:42 +0100

    Update feature comparison (#1557)
    * Add test
    
    * Move prices to tiers
    
    * make pricing test more specific
    
    * Try logging
    
    * Give up on pricing test
    
    * Revert workflow file changes
    
    * Fix indent on node version in workflow
    
    * Add enterprise tier with dupe info
    
    * Add enterprise tier
    
    * Tweaks
    
    * Remove enterprise

--- src/components/pricing/tiers.js
+++ src/components/pricing/tiers.js
@@ -1,72 +1,74 @@
 import { PAGE_PATHS } from '../../contactFormConstants';
 
-export const TIERS = {
-  teams: {
-    name: 'Teams',
-    description: 'For teams who want a home for their services, docs, runbooks, API specs and CI.',
-    minSeats: 50,
-    maxSeats: 150,
+export const TIERS = [{
+  name: 'Teams',
+  description: 'For teams who want a developer portal without sucking up engineering time.',
+  minSeats: 50,
+  maxSeats: 150,
 
-    ctaLabel: 'Start a free trial',
-    ctaLinkTo: PAGE_PATHS.freeTrial,
-    ctaHelpText: 'No credit card required',
+  ctaLabel: 'Start a free trial',
+  ctaLinkTo: PAGE_PATHS.freeTrial,
 
-    usdCentCostPerDevPerMonth: 2400,
-    eurCentCostPerDevPerMonth: 2000,
-    costQualifier: ' per dev/month',
-    customPricing: false,
-    pricingCondition: '50 to 150 developers. Unlimited entities.',
-
-    benefits: {
-      heading: "Key features",
-      bullets: [{
-        description: 'Backstage software catalog',
-      }, {
-        description: 'Monthly, automatic upgrades',
-      }, {
-        description: 'TechDocs & API specs',
-      }, {
-        description: 'Scaffolder golden paths',
-      }, {
-        description: '75+ Open-source Backstage plugins',
-      }, {
-        description: 'RAG AI & MCP Server Access (beta)',
-      }, {
-        description: 'Single sign on',
-      }, {
-        iconName: 'plus', description: 'Scorecards (optional paid extra)',
-      }, {
-        iconName: 'plus', description: 'Custom RBAC (optional paid extra)',
-      }],
-    },
+  usdCentCostPerDevPerMonth: 2400,
+  eurCentCostPerDevPerMonth: 2000,
+  costQualifier: ' per dev/month',
+  customPricing: false,
+  get pricingCondition() {
+    return `${this.minSeats} to ${this.maxSeats} developers. Unlimited entities.`;
   },
 
-  growth: {
-    name: 'Growth',
-    description: 'For excellent engineering orgs who want to maintain effectiveness through hypergrowth',
-    minSeats: 100,
-
-    ctaLabel: 'Request a demo',
-    ctaLinkTo: PAGE_PATHS.freeTrial,
-
-    customPricing: true,
-    pricingCondition: '100 developers or more. Unlimited entities.',
-
-    benefits: {
-      heading: "Everything in Teams, plus...",
-      bullets: [{
-        description: 'SLA',
-      }, {
-        description: 'Slack & MS Teams support',
-      }, {
-        description: 'Custom, private Backstage plugins',
-      }, {
-        description: 'REST API access',
-      }, {
-        description: 'Secure on-prem connection',
-      }, {
-        description: 'Usage analytics dashboard',
-      }],
-    },
+  benefits: {
+    heading: "Key features",
+    bullets: [{
+      description: 'Backstage software catalog',
+    }, {
+      description: 'Automatic Backstage upgrades',
+    }, {
+      description: 'TechDocs & API specs',
+    }, {
+      description: 'Self-service automation templates',
+    }, {
+      description: '75+ open-source plugins',
+    }, {
+      description: 'Single sign-on',
+    }, {
+      iconName: 'plus', description: 'Scorecards (optional paid extra)',
+    }],
   },
-};
+}, {
+
+  name: 'Growth',
+  description: 'For excellent engineering orgs who want to maintain effectiveness through hypergrowth',
+  minSeats: 100,
+
+  ctaLabel: 'Request a demo',
+  ctaLinkTo: PAGE_PATHS.freeTrial,
+
+  customPricing: true,
+  get pricingCondition() {
+    return `${this.minSeats} developers or more. Unlimited entities.`;
+  },
+
+  benefits: {
+    heading: "Everything in Teams, plus...",
+    bullets: [{
+      description: 'SLA',
+    }, {
+      description: 'Slack & MS Teams support',
+    }, {
+      description: 'RAG AI & MCP Server Access (beta)',
+    }, {
+      description: 'Custom, private Backstage plugins',
+    }, {
+      description: 'REST API access',
+    }, {
+      description: 'Advanced search',
+    }, {
+      description: 'Secure on-prem connection',
+    }, {
+      description: 'Usage analytics dashboard',
+    }, {
+      description: 'Custom RBAC',
+    }],
+  },
+}];
