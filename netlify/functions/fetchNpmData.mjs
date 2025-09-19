bc6b379a4a9545e9205c09b5558e5ec14a6db206
David Tuite <david@roadie.io>
2025-09-19 12:02:24 +0100

    CommonJS function

--- netlify/functions/fetchNpmData.mjs
+++
@@ -1,9 +0,0 @@
-import { storePackageData } from '../../src/npmPackageData';
-
-export default async () => {
-  const resp = await storePackageData({ authStrategy: 'automatic' });
-
-  return new Response({
-    resp,
-  });
-};
