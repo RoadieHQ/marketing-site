8ed732532d22433d86acf0932c88e3a76f66679c
David Tuite <david@roadie.io>
2025-09-21 10:25:10 +0100

    Added source cloudinary

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -304,6 +304,16 @@ const config = {
         display: 'swap',
       },
     },
+
+    {
+      resolve: `gatsby-source-cloudinary`,
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        resourceType: `image`,
+      }
+    }
   ],
 };
 
