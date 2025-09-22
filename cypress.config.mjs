f98641ee80a5e4131c8e3cda58a04473d6423f39
David Tuite <david@roadie.io>
2025-09-22 16:36:01 +0100

    Increase viewport

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8001',
+    // Has to be wide enough for some responsive elements which only render on desktop screens
+    // to render.
+    viewportWidth: 1200,
   },
 });
