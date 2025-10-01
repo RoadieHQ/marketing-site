228c82140ba9df129d9c64398c71de2151a7f6ff
David Tuite <david@roadie.io>
2025-10-01 19:00:19 +0100

    Remove Open Sans 3 font

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -305,7 +305,7 @@ const config = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`Montserrat:ital,wght@0,700`, `Source+Sans+3:ital,wght@0,400;0,700;1,400`],
+        fonts: [`Montserrat:ital,wght@0,700`],
         display: 'swap',
       },
     },
