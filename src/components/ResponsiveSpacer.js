f48a8fdadc760ab0b49844ed42cef00c9e0105ba
David Tuite <david@roadie.io>
2025-10-19 15:50:51 +0100

    Remove unused responsive spacer

--- src/components/ResponsiveSpacer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classnames from 'classnames';
-
-const ResponsiveSpacer = ({ children, className = {} }) => (
-  <div className={classnames('pb-4 md:py-6', className.root)}>{children}</div>
-);
-
-export default ResponsiveSpacer;
