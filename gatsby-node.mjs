17f0d998bcfa454a2ceba5be4a4d1bc4340422eb
gorkamolero <gorka.molero@gmail.com>
2025-10-20 17:30:30 +0200

    Default webpack behaviour

--- gatsby-node.mjs
+++ gatsby-node.mjs
@@ -223,9 +223,24 @@ export const onCreateWebpackConfig = ({ getConfig, actions, stage }) => {
 
     // Disable splitChunks completely to force all modules inline
     config.optimization.splitChunks = {
+      chunks: 'async',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
       cacheGroups: {
-        default: false,
-        vendors: false,
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
       },
     };
 
