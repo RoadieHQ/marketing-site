bb620852d8399be0b8adfbc88914010806c62238
David Tuite <david@roadie.io>
2025-10-23 11:38:19 +0100

    Add Backstage Weekly RSS feed

--- src/gatsby/rssFeedPlugin.mjs
+++ src/gatsby/rssFeedPlugin.mjs
@@ -96,6 +96,85 @@ const blogChangelogFeed = {
   title: 'Roadie Changelog',
 };
 
+
+const backstageWeeklyContent = (node) => {
+  let body = get(node, 'body.childMarkdownRemark.html');
+  const backstageChangelog = get(node, 'backstageChangelog.childMarkdownRemark.html');
+  const ecosystemChangelog = get(node, 'ecosystemChangelog.childMarkdownRemark.html');
+
+  if (backstageChangelog && backstageChangelog !== '') {
+    body += `<h2>Backstage Changelog</h2>`;
+    body += `<p>A quick look at changes that have been merged into Backstage in the past week.</p>`;
+    body += backstageChangelog;
+  }
+
+  if (ecosystemChangelog && ecosystemChangelog !== '') {
+    body += `<h2>EcosystemChangelog Changelog</h2>`;
+    body += `<p>Learn which plugins have received new features, bugfixes and breaking changes in the past week.</p>`;
+    body += ecosystemChangelog;
+  }
+
+  return body;
+};
+
+const backstageWeeklyFeed = {
+  serialize: ({ query: { site, issues } }) => {
+    return issues.edges.map(({ node }) => {
+      return {
+        title: `Backstage Weekly ${node.issueNumber} - ${node.title}`,
+        date: node.publishDate,
+        description: get(node, 'lead.childMarkdownRemark.rawMarkdownBody'),
+        url: site.siteMetadata.siteUrl + node.slug,
+        guid: site.siteMetadata.siteUrl + node.slug,
+        custom_elements: [{
+          'content:encoded': backstageWeeklyContent(node),
+        }],
+      };
+    });
+  },
+
+  query: `
+    query AllBackstageWeeklyForRss {
+      issues: allContentfulBackstageWeekly(
+        sort: {publishDate: DESC}
+      ) {
+        edges {
+          node {
+            lead {
+              childMarkdownRemark {
+                rawMarkdownBody
+              }
+            }
+            issueNumber
+            publishDate
+            slug
+            title
+            body {
+              childMarkdownRemark {
+                html
+              }
+            }
+            backstageChangelog {
+              childMarkdownRemark {
+                html
+              }
+            }
+            ecosystemChangelog {
+              childMarkdownRemark {
+                html
+              }
+            }
+          }
+        }
+      }
+    }
+  `,
+
+  output: '/backstage-weekly/rss.xml',
+  title: 'Backstage Weekly',
+};
+
+
 export default [{
   resolve: 'gatsby-plugin-feed',
   options: {
@@ -112,6 +191,6 @@ export default [{
       }
     `,
 
-    feeds: [blogFeed, blogChangelogFeed],
+    feeds: [blogFeed, blogChangelogFeed, backstageWeeklyFeed],
   },
 }];
